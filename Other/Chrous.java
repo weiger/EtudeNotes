cb4a819cb94628cc6b674b377c53158aa0128e1f
iloveoottat <yixiaomeng@gmail.com>
2014-09-21 23:13:43 -0700

    Update Chrous.java
    合唱队形

--- Other/Chrous.java
+++ Other/Chrous.java
@@ -43,5 +43,6 @@ public class Solution {
                 int[] data = {186,186,150,200,160,130,197,220};
                 int res = chrous(data);
                 System.out.println(res);
+                
             }
         }
