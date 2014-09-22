0f48aedcb04a6d6e750ffb352faf75b2361fb80a
iloveoottat <yixiaomeng@gmail.com>
2014-09-22 00:47:09 -0700

    Update LIS.java

--- Basic/LIS.java
+++ Basic/LIS.java
@@ -11,29 +11,35 @@ public class Solution {
         for(int i = 0;i<dp.length;i++){
             dp[i] = 1;
         }
+        int max = 0;
         for(int i = 1;i<data.length;i++){
             for(int j=0;j<i;j++){
                 if(data[i]>data[j])
                     dp[i] = Math.max(dp[i],dp[j]+1);
             }
+            if(dp[i]>max)
+                max = dp[i];
         }
-        return dp[dp.length-1];
+        return max;
     }
     public static int LIS2(int[] data){
         int[] dp = new int[data.length];
         for(int i = 0;i<dp.length;i++){
             dp[i] = 1;
         }
+        int max = 0;
         for(int i=0;i<data.length;i++){
             for(int j=i+1;j<data.length;j++){
                 if(data[j]>data[i])
                     dp[j] = Math.max(dp[j],dp[i]+1);
             }
+            if(dp[i]>max)
+                max = dp[i];
         }
-        return dp[dp.length-1];
+        return max;
     }
     public static void main(String[] args) {
-        int[] data = {1,7,3,5,9,4,8};
+        int[] data = {0,7,3,5,9,4,8,1,2,3,4,9};
         int res = LIS2(data);
         System.out.println(res);
     }
