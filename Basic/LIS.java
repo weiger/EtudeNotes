de17d5d1e4152b823d4008ca5d88bacf67a01a01
iloveoottat <yixiaomeng@gmail.com>
2014-09-23 00:38:48 -0700

    Update LIS.java

--- Basic/LIS.java
+++ Basic/LIS.java
@@ -5,42 +5,72 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Solution {
-    
-    public static int LIS1(int[] data){
+
+    public static int LIS1(int[] data) {
         int[] dp = new int[data.length];
-        for(int i = 0;i<dp.length;i++){
+        for (int i = 0; i < dp.length; i++) {
             dp[i] = 1;
         }
         int max = 0;
-        for(int i = 1;i<data.length;i++){
-            for(int j=0;j<i;j++){
-                if(data[i]>data[j])
-                    dp[i] = Math.max(dp[i],dp[j]+1);
+        for (int i = 1; i < data.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (data[i] > data[j])
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
             }
-            if(dp[i]>max)
+            if (dp[i] > max)
                 max = dp[i];
         }
         return max;
     }
-    public static int LIS2(int[] data){
+
+    public static int LIS2(int[] data) {
         int[] dp = new int[data.length];
-        for(int i = 0;i<dp.length;i++){
+        for (int i = 0; i < dp.length; i++) {
             dp[i] = 1;
         }
         int max = 0;
-        for(int i=0;i<data.length;i++){
-            for(int j=i+1;j<data.length;j++){
-                if(data[j]>data[i])
-                    dp[j] = Math.max(dp[j],dp[i]+1);
+        for (int i = 0; i < data.length; i++) {
+            for (int j = i + 1; j < data.length; j++) {
+                if (data[j] > data[i])
+                    dp[j] = Math.max(dp[j], dp[i] + 1);
             }
-            if(dp[i]>max)
+            if (dp[i] > max)
                 max = dp[i];
         }
         return max;
     }
+
+    public static int LIS3(int[] data) {
+        int[] helper = new int[data.length];
+        int k = 0;
+        helper[0] = data[0];
+        for (int i = 1; i < data.length; i++) {
+            if (data[i] >= helper[k])
+                helper[++k] = data[i];
+            else {
+                int pos = binarySearch(data, data[i], k);
+                helper[pos] = data[i];
+            }
+        }
+        return k + 1;
+    }
+
+    public static int binarySearch(int[] data, int num, int k) {
+        int beg = 0;
+        int end = k;
+        while (beg <= end) {
+            int mid = beg + (end - beg) / 2;
+            if (num >= data[mid])
+                beg = mid + 1;
+            else
+                end = mid - 1;
+        }
+        return beg;
+    }
+
     public static void main(String[] args) {
-        int[] data = {0,7,3,5,9,4,8,1,2,3,4,9};
-        int res = LIS2(data);
+        int[] data = { 1, 7, 3, 5, 9, 4, 8 };
+        int res = LIS3(data);
         System.out.println(res);
     }
 }
