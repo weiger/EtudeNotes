1f99b3bbd675f0f80a9b608f1b987c075e02dacd
iloveoottat <yixiaomeng@gmail.com>
2014-09-24 13:02:22 -0700

    Update MaximumSubarray.java

--- Leetcode/MaximumSubarray.java
+++ Leetcode/MaximumSubarray.java
@@ -3,7 +3,7 @@
  * For example, given the array [−2,1,−3,4,−1,2,1,−5,4],
  * the contiguous subarray [4,−1,2,1] has the largest sum = 6.
  */ 
-
+//Update the sum till now, if <0, update to 0, throw away all values before
 public class Solution {
     public int maxSubArray(int[] A) {
         int temp = 0;
