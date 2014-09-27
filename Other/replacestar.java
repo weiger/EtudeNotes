60474559f30bf5548492ef35ba57195d8ec9c387
Xiaomeng Yi <yixiaomeng@gmail.com>
2014-09-26 23:56:10 -0700

    将一个string 里的 * 换成0 或者 1

--- Other/replacestar.java
+++ Other/replacestar.java
@@ -1,35 +1,35 @@
 /*
-将一个string 里的 * 换成0 或者 1 ,输出所有的结果 
+  将一个string 里的 * 换成0 或者 1 ,输出所有的结果 
 */
 
+import java.util.ArrayList;
+
 public class Solution {
-    public static ArrayList<String> replacestart(String s){
+    public static ArrayList<String> replacestart(String s) {
         ArrayList<String> res = new ArrayList<String>();
-        if(s==null || s.length()==0)
+        if (s == null || s.length() == 0)
             return res;
-        helper(res,0,s);
+        helper(res, 0, s);
         return res;
     }
-    public static void helper(ArrayList<String> res,int deep,String buffer){
-        if(deep==buffer.length()){
-            if(!res.contains(buffer))
-                res.add(buffer);
+
+    public static void helper(ArrayList<String> res, int pos, String buffer) {
+        if (pos == buffer.length()) {
+            res.add(buffer);
             return;
         }
         char[] arr = buffer.toCharArray();
-        for(int i=deep;i<arr.length;i++){
-            if(arr[i]=='*'){
-                char temp = arr[i];
-                arr[i] = '0';
-                helper(res,i+1,new String(arr));
-                arr[i] = '1';
-                helper(res,i+1,new String(arr));
-        //arr[i] = temp;
-            }
-        }
+        if (arr[pos] == '*') {
+            arr[pos] = '0';
+            helper(res, pos + 1, new String(arr));
+            arr[pos] = '1';
+            helper(res, pos + 1, new String(arr));
+        } else
+        helper(res, pos + 1, new String(arr));
     }
+
     public static void main(String[] args) {
-        ArrayList<String> res = replacestart("c*a*b*");
+        ArrayList<String> res = replacestart("a*c*b");
         System.out.println(res);
-    }
+    } 
 }
