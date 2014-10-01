44ef7cf1e4055e336267af759ccae5c6e956f92b
SingleNumber <yixiaomeng@gmail.com>
2014-10-01 14:43:55 -0700

    Update NQueens.java

--- Leetcode/NQueens.java
+++ Leetcode/NQueens.java
@@ -26,43 +26,8 @@
  * ]
  * 
  */
-public class NQueens {
-    public ArrayList<String[]> solveNQueens(int n) {
-        ArrayList<String[]> res = new ArrayList<String[]>();
-        int[] board = new int[n];
-        placeQueenOnBoard(res, 0, board);
-        return res;
-    }
-
-    public void placeQueenOnBoard(ArrayList<String[]> res, int row, int[] board) {
-        int n = board.length;
-        if(row == n) {
-            String[] s = new String[n];
-            for(int i = 0; i < n; i++) {
-                s[i] = "";
-                for(int j = 0; j < n; j++) 
-                    s[i] += (j != board[i]) ? "." : "Q";
-            }
-            res.add(s);
-        } else {
-            for(int col = 0; col < n; col++) {
-                if(isSafePlace(row, col, board)) {
-                    board[row] = col;
-                    placeQueenOnBoard(res, row + 1, board);
-                    board[row] = 0;
-                }
-            }
-        }
-    }
-
-    public boolean isSafePlace(int row, int col, int[] board) {
-        for(int i = 0; i < row; i++)
-            if(board[i] == col || Math.abs(board[i] - col) == Math.abs(i - row))
-                return false;
-        return true;
-    }
-}
-//Same Idea
+ 
+ //Derive from N-queen 2
 public class Solution {
     public List<String[]> solveNQueens(int n) {
         ArrayList<String[]> res = new ArrayList<String[]>();
@@ -99,5 +64,44 @@ public class Solution {
     		return false;
     	}
     	return true;
+    }
+}
+
+ 
+ 
+public class NQueens {
+    public ArrayList<String[]> solveNQueens(int n) {
+        ArrayList<String[]> res = new ArrayList<String[]>();
+        int[] board = new int[n];
+        placeQueenOnBoard(res, 0, board);
+        return res;
+    }
+
+    public void placeQueenOnBoard(ArrayList<String[]> res, int row, int[] board) {
+        int n = board.length;
+        if(row == n) {
+            String[] s = new String[n];
+            for(int i = 0; i < n; i++) {
+                s[i] = "";
+                for(int j = 0; j < n; j++) 
+                    s[i] += (j != board[i]) ? "." : "Q";
+            }
+            res.add(s);
+        } else {
+            for(int col = 0; col < n; col++) {
+                if(isSafePlace(row, col, board)) {
+                    board[row] = col;
+                    placeQueenOnBoard(res, row + 1, board);
+                    board[row] = 0;
+                }
+            }
+        }
+    }
+
+    public boolean isSafePlace(int row, int col, int[] board) {
+        for(int i = 0; i < row; i++)
+            if(board[i] == col || Math.abs(board[i] - col) == Math.abs(i - row))
+                return false;
+        return true;
     }
 }
