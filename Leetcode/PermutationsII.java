8628d8919df6ea1fedd0818a0f6cdc6ecad66d43
SingleNumber <yixiaomeng@gmail.com>
2014-10-12 01:44:45 -0700

    Update PermutationsII.java

--- Leetcode/PermutationsII.java
+++ Leetcode/PermutationsII.java
@@ -5,26 +5,7 @@
  * [1,2,3] have the following permutations:
  * [1,2,3], [1,3,2], [2,1,3], [2,3,1], [3,1,2], and [3,2,1].
  */
-import java.util.*;
-
-public class PermutationsII {
-	public ArrayList<ArrayList<Integer>> permuteUnique(int[] num) {
-		ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();
-        res.add(new ArrayList<Integer>());
-		for(int i = 0; i < num.length; i++) {
-			HashSet<ArrayList<Integer>> set = new HashSet<ArrayList<Integer>>();
-			for(int j = 0; j < res.size(); j++) {
-				for(int k = 0; k <= res.get(j).size(); k++) {
-					ArrayList<Integer> list = new ArrayList<Integer>(res.get(j));
-					list.add(k, num[i]);
-					set.add(list);
-				}
-			}
-			res = new ArrayList<ArrayList<Integer>>(set);
-		}
-		return res;
-    }
-
+public class Solution {
     public List<List<Integer>> permuteUnique(int[] num) {
         List<List<Integer>> res = new ArrayList<>();
         if(num==null || num.length==0)
@@ -54,3 +35,24 @@ public class PermutationsII {
         }
     }
 }
+
+
+public class PermutationsII {
+	public ArrayList<ArrayList<Integer>> permuteUnique(int[] num) {
+		ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();
+        res.add(new ArrayList<Integer>());
+		for(int i = 0; i < num.length; i++) {
+			HashSet<ArrayList<Integer>> set = new HashSet<ArrayList<Integer>>();
+			for(int j = 0; j < res.size(); j++) {
+				for(int k = 0; k <= res.get(j).size(); k++) {
+					ArrayList<Integer> list = new ArrayList<Integer>(res.get(j));
+					list.add(k, num[i]);
+					set.add(list);
+				}
+			}
+			res = new ArrayList<ArrayList<Integer>>(set);
+		}
+		return res;
+    }
+}
+
