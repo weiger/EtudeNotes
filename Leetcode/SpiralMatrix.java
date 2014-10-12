c78ce1adad0d882d91615207eb36465388fe6111
SingleNumber <yixiaomeng@gmail.com>
2014-10-12 01:20:18 -0700

    Update SpiralMatrix.java

--- Leetcode/SpiralMatrix.java
+++ Leetcode/SpiralMatrix.java
@@ -11,31 +11,9 @@
  * ]
  * You should return [1,2,3,6,9,8,7,4,5].
  */
-public class SpiralMatrix {
-    public ArrayList<Integer> spiralOrder(int[][] matrix) {
-        ArrayList<Integer> result = new ArrayList<Integer>();
-        if(matrix.length == 0)
-            return result;
-        int M = matrix.length, N = matrix[0].length;     
-        int small = M < N ? M : N;
-        for(int i = 0; i < (small + 1) / 2; i++) {
-            for(int j  = i; j < N - i; j++) {
-                result.add(matrix[i][j]);
-            }
-            for(int j = i + 1; j < M - i; j++) {
-                result.add(matrix[j][N - 1 - i]);
-            }
-            for(int j = N - 2 - i; j > i && i != M - 1 - i; j--) {
-                result.add(matrix[M - 1 - i][j]);
-            }
-            for(int j = M - 1 - i; j > i && i != N - 1 - i; j--) {
-                result.add(matrix[j][i]);
-            }
-        }
-        return result;
-    }
-}
-//More Clear
+ /*More Clear
+ 外围转圈，当行和列为1是特殊处理，12 ,36,98,74, 这样转
+ */
 public class Solution {
     public ArrayList<Integer> spiralOrder(int[][] matrix) {
         ArrayList<Integer> res = new ArrayList<Integer>();
@@ -74,4 +52,29 @@ public class Solution {
 	return res;
         
         }
+}
+ 
+public class SpiralMatrix {
+    public ArrayList<Integer> spiralOrder(int[][] matrix) {
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        if(matrix.length == 0)
+            return result;
+        int M = matrix.length, N = matrix[0].length;     
+        int small = M < N ? M : N;
+        for(int i = 0; i < (small + 1) / 2; i++) {
+            for(int j  = i; j < N - i; j++) {
+                result.add(matrix[i][j]);
+            }
+            for(int j = i + 1; j < M - i; j++) {
+                result.add(matrix[j][N - 1 - i]);
+            }
+            for(int j = N - 2 - i; j > i && i != M - 1 - i; j--) {
+                result.add(matrix[M - 1 - i][j]);
+            }
+            for(int j = M - 1 - i; j > i && i != N - 1 - i; j--) {
+                result.add(matrix[j][i]);
+            }
+        }
+        return result;
+    }
 }
