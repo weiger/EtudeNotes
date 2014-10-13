5ace26bb963dfaabdeb0827fadba3649b1efc401
SingleNumber <yixiaomeng@gmail.com>
2014-10-13 03:46:03 -0700

    Update Triangle.java

--- hiho/Triangle.java
+++ hiho/Triangle.java
@@ -6,19 +6,19 @@ public class Main {
     public static void main(String[] args) {
         Scanner cin = new Scanner(System.in);
         int n = cin.nextInt();
-        int t = 0, j = 0, y = 0;
-        int[] dp = new int[n + 1];
-        for (int i = 1; i <= n; ++i)
-            for (t = j = 0; j < i; ++j) {
+        int[] dp = new int[n+1];
+        for(int i=1;i<=n;i++){
+            for(int j=0,t=0;j<i;j++){
                 int x = cin.nextInt();
-                y = dp[j];
-                dp[j] = (y > t ? y : t) + x;
-                t = y;
+                int tmp = dp[j];
+                dp[j] = Math.max(t, dp[j]) + x;
+                t = tmp;
             }
-            int res = 0;
-            for (int x : dp) {
-                res = Math.max(res, x);
-            }
-            System.out.println(res);
         }
+        int res = 0;
+        for(int i=1;i<=n;i++)
+            res = Math.max(res, dp[i]);
+        System.out.println(res);
     }
+}
+
