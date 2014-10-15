7e056acf6fc3113ca08a0679896ee38fdfa1ee13
SingleNumber <yixiaomeng@gmail.com>
2014-10-15 13:12:21 -0700

    Update RemoveDuplicatesFromSortedList.java

--- Leetcode/RemoveDuplicatesFromSortedList.java
+++ Leetcode/RemoveDuplicatesFromSortedList.java
@@ -17,6 +17,23 @@
  *     }
  * }
  */
+ 
+ public class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if(head==null || head.next==null)
+            return head;
+        ListNode cur = head;
+        while(cur.next!=null){
+            if(cur.val == cur.next.val){
+                cur.next = cur.next.next;
+                continue;
+            }
+            cur = cur.next;
+        }
+        return head;
+    }
+}
+ 
 public class Solution {
     public ListNode deleteDuplicates(ListNode head) {
         ListNode cur = head;
