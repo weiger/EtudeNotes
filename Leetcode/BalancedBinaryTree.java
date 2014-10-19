4272838bf8a26c1d52a9f6d3daea545787355cf9
SingleNumber <yixiaomeng@gmail.com>
2014-10-18 23:46:59 -0700

    Update BalancedBinaryTree.java

--- Leetcode/BalancedBinaryTree.java
+++ Leetcode/BalancedBinaryTree.java
@@ -15,17 +15,16 @@
  */
 //Be careful of meaning of balancedtree is that each node in tree is balanced
 public class BalancedBinaryTree {
-    public int depth(TreeNode root) {
-        if(root == null)
-            return 0;
-        return 1 + Math.max(depth(root.left), depth(root.right));
-    }
-
     public boolean isBalanced(TreeNode root) {
         if(root == null)
             return true;
         return Math.abs(depth(root.left) - depth(root.right)) <= 1 
             && isBalanced(root.left) && isBalanced(root.right);
+    }
+    public int depth(TreeNode root) {
+        if(root == null)
+            return 0;
+        return 1 + Math.max(depth(root.left), depth(root.right));
     }
 
 /*****************************************************************************/
