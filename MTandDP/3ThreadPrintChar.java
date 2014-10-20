86d2255a933ea1e9b7ae501297f29e6bebf271dc
SingleNumber <yixiaomeng@gmail.com>
2014-10-20 03:26:11 -0700

    Update 3ThreadPrintChar.java

--- MTandDP/3ThreadPrintChar.java
+++ MTandDP/3ThreadPrintChar.java
@@ -45,3 +45,88 @@ public class SleepExample extends Thread {
     }
 
 }
+//Method 2
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class PrintThreadExample {
+
+    public static void main(String[] args) {
+        PrintThreadExample example = new PrintThreadExample();
+
+        LetterPrinter letterPrinter = example.new LetterPrinter();
+
+        ExecutorService service = Executors.newFixedThreadPool(3);
+
+        service.execute(example.new PrintRunnable(letterPrinter, 'A'));
+        service.execute(example.new PrintRunnable(letterPrinter, 'B'));
+        service.execute(example.new PrintRunnable(letterPrinter, 'C'));
+
+        service.shutdown();
+    }
+
+    private class LetterPrinter {
+        private char letter = 'A';
+
+        public void print() {
+            System.out.print(letter);
+        /*if ('C' == letter) {
+        System.out.println();
+    }*/
+}
+
+public void nextLetter() {
+    switch (letter) {
+        case 'A':
+        letter = 'B';
+        break;
+        case 'B':
+        letter = 'C';
+        break;
+        case 'C':
+        letter = 'A';
+        break;
+    }
+}
+
+public char getLetter() {
+    return letter;
+}
+
+}
+
+private class PrintRunnable implements Runnable {
+
+    private LetterPrinter letterPrinter = null;
+
+    private char letter = ' ';
+
+    public PrintRunnable(LetterPrinter letterPrinter, char letter) {
+        this.letterPrinter = letterPrinter;
+        this.letter = letter;
+    }
+
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            synchronized (letterPrinter) {
+                while (letter != letterPrinter.getLetter()) {
+                    try {
+                        letterPrinter.wait();
+                    } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+
+                letterPrinter.print();
+                letterPrinter.nextLetter();
+                letterPrinter.notifyAll();
+
+            }
+        }
+
+    }
+
+}
+
+}
