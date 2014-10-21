df88c4e1edcaae60da828a4c1db33a50ad84fee2
SingleNumber <yixiaomeng@gmail.com>
2014-10-21 00:38:47 -0700

    Create FindMinimuminRotatedSortedArrayII.java

---
+++ Leetcode/FindMinimuminRotatedSortedArrayII.java
@@ -0,0 +1,20 @@
+public class Solution {
+    public int findMin(int[] num) {
+        if(num==null || num.length==0)
+            return -1;
+        int beg = 0;
+        int end = num.length - 1;
+        while(beg<end){
+            if(num[beg]<num[end])
+                return num[beg];
+            int mid = beg + (end - beg) / 2;
+            if(num[mid]>num[beg])
+                beg = mid+1;
+            else if(num[mid]<num[beg])
+                end = mid;
+            else
+                beg++;
+        }
+        return num[beg];
+    }
+}
