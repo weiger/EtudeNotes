9f55d309fcd6cf579319c348c63f831714ee5b5b
SingleNumber <yixiaomeng@gmail.com>
2014-10-21 19:52:41 -0700

    Create Q2_3.java

---
+++ cc150/Q2_3.java
@@ -0,0 +1,77 @@
+
+public class Q2_3 {
+    public static boolean deleteNode(ListNode n){
+        if(n.next==null)
+            return false;
+        ListNode next = n.next;
+        n.val = next.val;
+        n.next = next.next;
+        return true;
+        
+    }
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list.randomList(10);
+        list.printList();
+        deleteNode(list.head.next.next);
+        list.printList();
+    }
+
+}
+class LinkedList {
+    ListNode head;
+    ListNode last;
+
+    LinkedList() {
+        head = null;
+        last = null;
+    }
+
+    public void insertLast(int val) {
+        ListNode newnode = new ListNode(val);
+        if (head == null) {
+            head = newnode;
+            last = head;
+        } else {
+            last.next = newnode;
+        }
+    }
+
+    public void insertFirst(int val) {
+        ListNode newnode = new ListNode(val);
+        if (head == null) {
+            head = newnode;
+            last = head;
+        } else {
+            newnode.next = head;
+            head = newnode;
+        }
+    }
+
+    public void randomList(int n) {
+        head = null;
+        last = null;
+        for (int i = 0; i < n; i++) {
+            insertFirst((int) (10 * Math.random()));
+        }
+    }
+
+    public void printList() {
+        ListNode cur = head;
+        while (cur != null) {
+            System.out.print(cur.val + " ");
+            cur = cur.next;
+        }
+        System.out.println();
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+        next = null;
+    }
+}
