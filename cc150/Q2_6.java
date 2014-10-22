e895c07dacddc8d48636fbfc911f6faab1893f9a
SingleNumber <yixiaomeng@gmail.com>
2014-10-21 23:21:44 -0700

    Create Q2_6.java

---
+++ cc150/Q2_6.java
@@ -0,0 +1,96 @@
+public class Q2_4 {
+    
+    public static ListNode findCycle(ListNode head){
+        if(head==null)
+            return null;
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast!=null && fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow==fast)
+                break;
+        }
+        if(fast==null || fast.next==null)
+            return null;
+        slow = head;
+        while(slow!=fast){
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return fast;
+    }
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list.randomList(20);
+        int n = (int)(Math.random() * 20);
+        ListNode xnode = list.head;
+        while(n>1){
+            xnode = xnode.next;
+            n--;
+        }
+        list.last.next = xnode;
+        System.out.println("The beginning of cycle is " + xnode.val);
+        ListNode beg = findCycle(list.head);
+        System.out.println("The beginning of cycle is " + beg.val);
+    }
+
+}
+
+class LinkedList {
+    ListNode head;
+    ListNode last;
+
+    LinkedList() {
+        head = null;
+        last = null;
+    }
+
+    public void insertLast(int val) {
+        ListNode newnode = new ListNode(val);
+        if (head == null) {
+            head = newnode;
+            last = head;
+        } else {
+            last.next = newnode;
+        }
+    }
+
+    public void insertFirst(int val) {
+        ListNode newnode = new ListNode(val);
+        if (head == null) {
+            head = newnode;
+            last = head;
+        } else {
+            newnode.next = head;
+            head = newnode;
+        }
+    }
+
+    public void randomList(int n) {
+        head = null;
+        last = null;
+        for (int i = 0; i < n; i++) {
+            insertFirst((int) (10 * Math.random()));
+        }
+    }
+
+    public void printList() {
+        ListNode cur = head;
+        while (cur != null) {
+            System.out.print(cur.val + " ");
+            cur = cur.next;
+        }
+        System.out.println();
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+        next = null;
+    }
+}
