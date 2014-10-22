092ba379c5b2675924ae0ffa1b98ce9769423088
SingleNumber <yixiaomeng@gmail.com>
2014-10-22 01:33:55 -0700

    Create POJ3580.java

---
+++ POJ/POJ3580.java
@@ -0,0 +1,7 @@
+/*http://poj.org/problem?id=3580
+http://poj.org/problem?id=3580
+http://codeforces.com/blog/entry/6939
+http://wenku.baidu.com/view/db0f77661ed9ad51f01df23f.html?qq-pf-to=pcqq.group
+
+
+*/
