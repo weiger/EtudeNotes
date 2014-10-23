54d37956fd5f9e126f2d8c0ce115ff96c8558f06
SingleNumber <yixiaomeng@gmail.com>
2014-10-23 14:21:58 -0700

    Update BestTimeToBuyAndSellStock.java

--- Leetcode/BestTimeToBuyAndSellStock.java
+++ Leetcode/BestTimeToBuyAndSellStock.java
@@ -4,6 +4,23 @@
  * If you were only permitted to complete at most one transaction (ie, buy one and sell one 
  * share of the stock), design an algorithm to find the maximum profit.
  */
+ public class Solution {
+    public int maxProfit(int[] prices) {
+        if(prices.length==0 || prices==null)
+            return 0;
+        int max = 0;
+        int min = prices[0];
+        int res = 0;
+        for(int i=1;i<prices.length;i++){
+            res = Math.max(res,prices[i]-min);
+            min = Math.min(min,prices[i]);
+        }
+        return res;
+    }
+}
+ 
+ 
+ 
 public class BestTimeToBuyAndSellStock {
 /******************************** updated 2013/11/20 *****************************/
     public int maxProfit(int[] prices) {
