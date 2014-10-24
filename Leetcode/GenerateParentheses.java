663e45b1a07e07b4b368e7fbabb18ddad5b0348b
SingleNumber <yixiaomeng@gmail.com>
2014-10-23 20:51:30 -0700

    Update GenerateParentheses.java

--- Leetcode/GenerateParentheses.java
+++ Leetcode/GenerateParentheses.java
@@ -6,26 +6,26 @@
 
  * "((()))", "(()())", "(())()", "()(())", "()()()" 
  */
-public class GenerateParentheses {
-/************************** updated 2013/12/10 *******************************/
-
-    public ArrayList<String> generateParenthesis(int n) {
-        ArrayList<String> res = new ArrayList<String>();
-        helper(res, "", n, n);
+public class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> res = new ArrayList<>();
+        helper(res,n,n,"");
         return res;
     }
-    
-    public void helper(ArrayList<String> array, String s, int left, int right) {
-        if(left == 0 && right == 0)
-            array.add(s);
-        if(left > 0)
-            helper(array, s + "(", left - 1, right);
-        if(left < right)
-            helper(array, s + ")", left, right - 1);
+    public void helper(List<String> res,int left,int right,String str){
+        if(left==0 && right==0){
+            res.add(str);
+            return; 
+        }
+        if(left>0)
+            helper(res,left-1,right,str+"(");
+        if(left<right)
+            helper(res,left,right-1,str+")");
     }
-
-/*****************************************************************************/
-
+} 
+ 
+ 
+public class GenerateParentheses {
     public ArrayList<String> generateParenthesis(int n) {
         ArrayList<String> res = new ArrayList<String>();
         if(n == 0) 
