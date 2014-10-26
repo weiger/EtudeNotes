7c1dc3e69879202bd9e13ff08f7af58a135f88be
SingleNumber <yixiaomeng@gmail.com>
2014-10-26 02:56:52 -0700

    Update ValidNumber.java

--- Leetcode/ValidNumber.java
+++ Leetcode/ValidNumber.java
@@ -10,6 +10,20 @@
  * Note: It is intended for the problem statement to be ambiguous. You should gather all requirements up 
  * front before implementing one.
  */
+ public class Solution {
+    public boolean isNumber(String s) {
+        if(s.trim().isEmpty()){  
+            return false;  
+        }  
+        String regex = "[-+]?(\\d+\\.?|\\.\\d+)\\d*(e[-+]?\\d+)?";  
+        if(s.trim().matches(regex)){  
+            return true;  
+        }else{  
+            return false;  
+        }  
+    }
+}
+ 
 public class ValidNumber {
     public boolean isNumber(String s) {
         s = s.trim();
