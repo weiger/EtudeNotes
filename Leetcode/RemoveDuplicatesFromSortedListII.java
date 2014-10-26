cadfe6748004da219e9c27777c6869acaa71e66d
SingleNumber <yixiaomeng@gmail.com>
2014-10-26 04:34:30 -0700

    Update RemoveDuplicatesFromSortedListII.java

--- Leetcode/RemoveDuplicatesFromSortedListII.java
+++ Leetcode/RemoveDuplicatesFromSortedListII.java
@@ -17,6 +17,30 @@
  *     }
  * }
  */
+ public class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if(head==null || head.next==null)
+            return head;
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode pre = dummy;
+        ListNode cur = head;
+        while(cur!=null){
+            if(cur.next!=null && cur.val == cur.next.val){
+                while(cur.next!=null && cur.val == cur.next.val){
+                    cur = cur.next;
+                }
+                pre.next = cur.next;
+                cur = cur.next;
+            }else{
+                pre = pre.next;
+                cur = cur.next;
+            }
+        }
+        return dummy.next;
+    }
+}
+ 
 public class RemoveDuplicatesFromSortedListII {
 
     public ListNode deleteDuplicates(ListNode head) {
