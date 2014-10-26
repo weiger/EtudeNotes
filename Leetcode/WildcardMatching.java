6ee628ad33350fc7a1b11607ac77d5688a21cb8f
SingleNumber <yixiaomeng@gmail.com>
2014-10-26 01:52:16 -0700

    Update WildcardMatching.java

--- Leetcode/WildcardMatching.java
+++ Leetcode/WildcardMatching.java
@@ -18,52 +18,7 @@
  * isMatch("ab", "?*") ? true
  * isMatch("aab", "c*a*b") ? false 
  */
-public class WildcardMatching {
-    /* This DP method can not access the judge large for Memory Limit Exceeded 
-    public boolean isMatch(String s, String p) {
-        boolean[][] dp = new boolean[s.length() + 1][ p.length() + 1];
-        dp[0][0] = true;
-        for(int i = 1; i <= s.length(); i++) 
-            dp[i][0] = (s.length() == 0);
-        for(int i = 1; i <= p.length(); i++)
-            dp[0][i] = dp[0][i - 1] && (p.charAt(i - 1) == '*');
-        for(int i = 1; i <= s.length(); i++) {
-            for(int j = 1; j <= p.length(); j++) {
-                if(p.charAt(j - 1) != '?' && p.charAt(j - 1) != '*')
-                    dp[i][j] = (s.charAt(i - 1) == p.charAt(j - 1)) && dp[i - 1][j - 1];
-                if(p.charAt(j - 1) == '?')
-                    dp[i][j] = dp[i - 1][j - 1];
-                if(p.charAt(j - 1) == '*')
-                    dp[i][j] = dp[i][j - 1] || dp[i - 1][j];
-            }
-        }
-        return dp[s.length()][p.length()];
-    } 
-    */
-
-    /* This recursion can not access the judge large for Time Limit Exceeded 
-    public boolean isMatch(String s, String p) {
-        if(p.length() == 0)
-            return s.length() == 0;
-        if(s.length() == 0)
-            return p.length() == 0 || isAllStar(p);
-        if((s.charAt(0) == p.charAt(0)) || p.charAt(0) == '?')
-            return isMatch(s.substring(1), p.substring(1));
-        if(p.charAt(0) == '*')
-            return isMatch(s.substring(1), p) || isMatch(s, p.substring(1));
-        else
-            return false;
-    }
-
-    public boolean isAllStar(String s) {
-        for(int i = 0; i < s.length(); i++) {
-            if(s.charAt(i) != '*')
-                return false;
-        }
-        return true;
-    }
-    */
-//Star for tracking last '*' and imark for last i value when '*' appeared, keep on matching until not matching
+ //Star for tracking last '*' and imark for last i value when '*' appeared, keep on matching until not matching
 //if we dont have '*', matching failed otherwise keep going
 public class Solution {
     public boolean isMatch(String s, String p) {
@@ -88,3 +43,65 @@ public class Solution {
         return j == p.length();
     }
 }
+ 
+ public class Solution {
+    public boolean isMatch(String s, String p) {
+        int m = s.length();
+        int n = p.length();
+        int count = 0;
+        //only for TLE
+        for(int i = 0;i<p.length();i++){
+            if(p.charAt(i)!='*')
+                count++;
+            if(count>m)
+                return false;
+        }
+        boolean[][] dp = new boolean[m + 1][n + 1];
+        for (int i = 0; i <= m; i++) {
+            for (int j = 0; j <= n; j++) {
+                if (i == 0 && j == 0) {
+                    dp[i][j] = true;
+                }
+                else if (i != 0 && j == 0) {
+                    dp[i][j] = false;
+                }
+                else if (i == 0 && j != 0) {
+                    dp[i][j] = dp[i][j-1] && p.charAt(j-1)=='*';
+                }else{
+                    if(p.charAt(j-1)==s.charAt(i-1) || p.charAt(j-1)=='?'){
+                        dp[i][j] = dp[i-1][j-1];
+                    }else if(p.charAt(j-1)=='*'){
+                        dp[i][j] = dp[i-1][j-1]||dp[i][j-1]||dp[i-1][j];
+                    }
+                }
+            }
+        }
+        return dp[m][n];
+    }
+}
+ 
+ 
+public class WildcardMatching {
+    /* This recursion can not access the judge large for Time Limit Exceeded 
+    public boolean isMatch(String s, String p) {
+        if(p.length() == 0)
+            return s.length() == 0;
+        if(s.length() == 0)
+            return p.length() == 0 || isAllStar(p);
+        if((s.charAt(0) == p.charAt(0)) || p.charAt(0) == '?')
+            return isMatch(s.substring(1), p.substring(1));
+        if(p.charAt(0) == '*')
+            return isMatch(s.substring(1), p) || isMatch(s, p.substring(1));
+        else
+            return false;
+    }
+
+    public boolean isAllStar(String s) {
+        for(int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) != '*')
+                return false;
+        }
+        return true;
+    }
+    */
+
