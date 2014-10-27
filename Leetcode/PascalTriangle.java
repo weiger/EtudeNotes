ea7f3c54cf234db7ec5268810358dccedfe9c1b4
SingleNumber <yixiaomeng@gmail.com>
2014-10-27 15:36:15 -0700

    Update PascalTriangle.java

--- Leetcode/PascalTriangle.java
+++ Leetcode/PascalTriangle.java
@@ -12,6 +12,36 @@
  *  [1,4,6,4,1]
  * ]
  */
+ /*
+ [1],
+[1,1],
+[1,2,1],
+[1,3,3,1],
+[1,4,6,4,1]
+ more easier in t
+ */
+ public class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> res = new ArrayList<>();
+        if(numRows<=0)
+            return res;
+        List<Integer> first = new ArrayList<>();
+        first.add(1);
+        res.add(first);
+        for(int i=1;i<numRows;i++){
+            List<Integer> list = new ArrayList<>();
+            list.add(1);
+            for(int j=1;j<i;j++){
+                list.add(res.get(i-1).get(j)+res.get(i-1).get(j-1));
+            }
+            list.add(1);
+            res.add(list);
+        }
+        return res;
+    }
+}
+ 
+ 
 public class PascalTriangle {
     public ArrayList<ArrayList<Integer>> generate(int numRows) {
         ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();
@@ -31,23 +61,3 @@ public class PascalTriangle {
         return res;
     }
 }
-
-/*public static ArrayList<ArrayList<Integer>> generate(int numRows)
-{
-	ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();
-	ArrayList<Integer> first = new ArrayList<Integer>();
-	if(numRows<=0) return res;
-	first.add(1);
-	res.add(first);
-	for(int i=1;i<numRows;i++){
-		ArrayList<Integer> next = new ArrayList<Integer>();
-		ArrayList<Integer> prev = res.get(i-1);
-		next.add(1);
-		for(int j=1;j<i;j++){
-			next.add(prev.get(j-1)+prev.get(j));
-		}
-		next.add(1);
-		res.add(next);
-	}
-	return res;
-}*/
