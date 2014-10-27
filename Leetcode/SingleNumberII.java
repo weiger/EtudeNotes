b9790e6047ff3f3ee7dbab600bee1ef28b2ce003
SingleNumber <yixiaomeng@gmail.com>
2014-10-27 16:05:42 -0700

    Update SingleNumberII.java

--- Leetcode/SingleNumberII.java
+++ Leetcode/SingleNumberII.java
@@ -4,31 +4,38 @@
  * Note:
  * Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
  */
-public class SingleNumberII {
-/*  public int singleNumber(int[] A) {
-        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
-        for(int i = 0; i < A.length; i++) {
-            map.put(A[i], map.containsKey(A[i]) ? map.get(A[i]) + 1 : 1);
-        }
-        int i = 0; 
-        while(map.get(A[i]) == 3) {
-            i++;
-        }
-        return A[i];
-    }
-
+ public class Solution {
     public int singleNumber(int[] A) {
-        int[] count = new int[32];
-        int result = 0;
-        for(int i = 0; i < 32; i++) {
-            for(int j = 0; j < A.length; j++) {
-                count[i] += (A[j] >> i) & 1;
+        HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
+        for(int x:A){
+            if(!map.containsKey(x)){
+                map.put(x,1);
+            }else{
+                map.put(x,map.get(x)+1);
             }
-            count[i] %= 3;
-            result |= count[i] << i;
         }
-        return result;
+        for(int x:A){
+            if(map.get(x)!=3)
+                return x;
+        }
+        return -1;
     }
+}
+ 
+public class Solution {
+    public int singleNumber(int[] A) {
+        int res = 0;
+        int[] rec = new int[32];
+        for(int i=0;i<32;i++){
+            for(int j=0;j<A.length;j++){
+                rec[i] += (A[j]>>i)&1;
+            }
+            rec[i] = rec[i]%3;
+            res |= rec[i]<<i;
+        }
+        return res;
+    }
+}
 */
 /* 如果用二进制来表达数组中每一个数字的话，如果我们遍历一遍数组中的所有的数，即将数组中所有数的二进制表达的每位上的“1”都数一遍，那么除了需要找的那个只出现一次的整数外，其他所有的数字二进制表示中每一位“1”都出现了3k次（k为非负整数）。
 如果能有办法将所有的二进制表达中这些3k个“1”清零，那么剩下的二进制表达就表示了需要找到的数。
