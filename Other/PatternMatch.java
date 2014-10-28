6e471932ce7311cac1fd7ec775e1c06d0bd24cd3
SingleNumber <yixiaomeng@gmail.com>
2014-10-27 18:54:31 -0700

    Update PatternMatch.java

--- Other/PatternMatch.java
+++ Other/PatternMatch.java
@@ -10,33 +10,41 @@ import java.util.Map;
 public class Solution {
 
     public static void main(String[] args) {
-        String pattern = "abab";
-        String input = "redblueredblue";
-        Map<Character, String> map = new HashMap<Character, String>();
-        System.out.println(isMatch(pattern, 0, input, 0, map));
+    	String pattern = "abab";
+    	String input = "redblueredblue";
+    	Map<Character, String> map = new HashMap<Character, String>();
+    	System.out.println(isMatch(pattern, 0, input, 0, map));
+    	map.clear();
+    	System.out.println(isMatch("aaaa", 0, "asdasdasdasd", 0, map));
+    	map.clear();
+    	System.out.println(isMatch("aabb", 0, "xyzabcxzyabc", 0, map));
     }
 
-    public static boolean isMatch(String p, int pidx, String input, int start,
-        Map<Character, String> map) {
-    if (start == input.length() && pidx == p.length())
-        return true;
-    char c = p.charAt(pidx);
-    if (map.get(c) != null) {
-        int len = map.get(c).length();
-        if (start + len > input.length()
-            || !input.substring(start, start + len).equals(map.get(c))) {
-            return false;
-        } else
-            return isMatch(p, pidx + 1, input, start + len, map);
-    } else {
-        for (int len = 1; len <= input.length() - start; len++) {
-            map.put(c, input.substring(start, start + len));
-            if (isMatch(p, pidx + 1, input, start + len, map))
-                return true;
-            map.remove(c);
-        }
+    public static boolean isMatch(String pattern, int p, String str, int s,
+	    Map<Character, String> map) {
+	if (p == pattern.length() && s == str.length())
+	    return true;
+	char c = pattern.charAt(p);
+	if (map.containsKey(c)) {
+	    int len = map.get(c).length();
+
+	    if (s + len > str.length()
+		    || !str.substring(s, s + len).equals(map.get(c))) {
+		    return false;
+	    } else {
+		    return isMatch(pattern, p + 1, str, s + len, map);
+	    }
+	} else {
+	    for (int len = 1; len <= str.length() - s; len++) {
+    		map.put(c, str.substring(s, s + len));
+    		if (isMatch(pattern, p + 1, str, p + len, map))
+    		    return true;
+    		map.remove(c);
+	    }
+	}
+	    return false;
     }
-    return false;
-    }
+
 }
+
 
