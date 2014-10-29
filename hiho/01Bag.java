a238a0f48cde407496d3576d3d6f003ceadf896e
SingleNumber <yixiaomeng@gmail.com>
2014-10-29 03:24:03 -0700

    Update 01Bag.java

--- hiho/01Bag.java
+++ hiho/01Bag.java
@@ -26,3 +26,33 @@ public class Main {
         System.out.println(dp[n][sum]);
     }
 }
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Scanner cin = new Scanner(System.in);
+	int n = cin.nextInt();
+	int sum = cin.nextInt();
+	int[] need = new int[n+1];
+	int[] value = new int[n+1];
+	for(int i=1;i<=n;i++){
+	    need[i] = cin.nextInt();
+	    value[i] = cin.nextInt();
+	}
+	int[][] dp = new int[n+1][sum+1];
+	for(int i=0;i<=sum;i++)
+	    dp[0][i] = 0;
+	for(int i=1;i<=n;i++){
+	    for(int j=1;j<=sum;j++){
+		if(j<need[i])
+		    dp[i][j] = dp[i-1][j];
+		else
+		    dp[i][j] = Math.max(dp[i-1][j-need[i]]+value[i], dp[i-1][j]);
+	    }
+	}
+	System.out.println(dp[n][sum]);
+    }
+}
+
