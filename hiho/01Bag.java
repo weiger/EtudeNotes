3e162221299187d11fd538cea6e34d5f57ba4962
SingleNumber <yixiaomeng@gmail.com>
2014-10-29 03:42:07 -0700

    Update 01Bag.java

--- hiho/01Bag.java
+++ hiho/01Bag.java
@@ -64,23 +64,19 @@ public class Main {
         Scanner cin = new Scanner(System.in);
         int n = cin.nextInt();
         int sum = cin.nextInt();
-        int[] need = new int[n];
-        int[] value = new int[n];
-        for(int i=0;i<n;i++){
+        int[] need = new int[n+1];
+        int[] value = new int[n+1];
+        for(int i=1;i<=n;i++){
             need[i] = cin.nextInt();
             value[i] = cin.nextInt();
         }
-        int[][] dp = new int[n+1][sum+1];
-        for(int i=0;i<=sum;i++)
-            dp[0][i] = 0;
+        int[] dp = new int[sum+1];
+
         for(int i=1;i<=n;i++){
-            for(int j=0;j<=sum;j++){
-                if(j<need[i-1])
-                    dp[i][j] = dp[i-1][j];
-                else
-                    dp[i][j] = Math.max(dp[i-1][j-need[i-1]]+value[i-1], dp[i-1][j]);
+            for(int j=sum;j>=need[i];j--){
+                dp[j] = Math.max(dp[j],dp[j-need[i]]+value[i]);
             }
         }
-        System.out.println(dp[n][sum]);
+        System.out.println(dp[sum]);
     }
 }
