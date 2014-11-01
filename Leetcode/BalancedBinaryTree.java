c634d1e5d071d51dd3430bc71da0fc5c2cccf098
SingleNumber <yixiaomeng@gmail.com>
2014-10-31 17:43:25 -0700

    Update BalancedBinaryTree.java

--- Leetcode/BalancedBinaryTree.java
+++ Leetcode/BalancedBinaryTree.java
@@ -14,19 +14,34 @@
  * }
  */
 //Be careful of meaning of balancedtree is that each node in tree is balanced
-public class BalancedBinaryTree {
+public class Solution {
     public boolean isBalanced(TreeNode root) {
-        if(root == null)
-            return true;
-        return Math.abs(depth(root.left) - depth(root.right)) <= 1 
-            && isBalanced(root.left) && isBalanced(root.right);
+        if (root == null)
+	        return true;
+	    return Math.abs(getHeight(root.left) - getHeight(root.right)) <= 1 && isBalanced(root.left) && isBalanced(root.right);
+		
     }
-    public int depth(TreeNode root) {
-        if(root == null)
-            return 0;
-        return 1 + Math.max(depth(root.left), depth(root.right));
+    public int getHeight(TreeNode p){
+        if (p == null)
+    	    return 0;
+	    return 1 + Math.max(getHeight(p.left), getHeight(p.right));
     }
+}
 
+public class Solution {
+    public boolean isBalanced(TreeNode root) {
+        return getHeight(root)>-1;
+    }
+    public int getHeight(TreeNode p){
+        if(p==null)
+            return 0;
+        int left = getHeight(p.left);
+        int right = getHeight(p.right);
+        if(left<0 || right<0 || Math.abs(left-right)>1)
+            return -1;
+        return 1 + Math.max(left,right);
+    }
+}
 /*****************************************************************************/
 
     public boolean isBalanced(TreeNode root) {
@@ -41,27 +56,5 @@ public class BalancedBinaryTree {
         if(leftDepth < 0 || rightDepth < 0 || Math.abs(leftDepth - rightDepth) > 1)
             return -1;
         return 1 + Math.max(leftDepth, rightDepth);
-    }
-}
-public class Solution {
-    public boolean isBalanced(TreeNode root) {
-        if(getHeight(root)==-1)
-            return false;
-        else
-            return true;
-    }
-    public int getHeight(TreeNode p){
-        if(p==null)
-            return 0;
-        int leftheight = getHeight(p.left);
-        if(leftheight==-1)
-            return -1;
-        int rightheight = getHeight(p.right);
-        if(rightheight==-1)
-            return -1; 
-        if(Math.abs(leftheight-rightheight)>1)
-            return -1;
-        else
-            return Math.max(leftheight,rightheight)+1;
     }
 }
