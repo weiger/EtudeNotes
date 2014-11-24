c24984e2f57eb35464c4a7e07d841943c05c185b
SingleNumber <yixiaomeng@gmail.com>
2014-11-23 22:23:30 -0800

    Create climbingstairs.java

---
+++ Leetcode/Lint/climbingstairs.java
@@ -0,0 +1,23 @@
+/*
+You are climbing a stair case. It takes n steps to reach to the top.
+
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+*/
+
+public class Solution {
+    /**
+     * @param n: An integer
+     * @return: An integer
+     */
+    public int climbStairs(int n) {
+        if(n<1)
+            return 0;
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i=2;i<=n;i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+    }
+}
