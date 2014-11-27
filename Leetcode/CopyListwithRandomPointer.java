4a59a0c80ee96ca8f5c4855ef29be48d4612cba4
SingleNumber <yixiaomeng@gmail.com>
2014-11-26 17:40:51 -0800

    Update CopyListwithRandomPointer.java

--- Leetcode/CopyListwithRandomPointer.java
+++ Leetcode/CopyListwithRandomPointer.java
@@ -36,3 +36,47 @@ public class Solution {
         return copy;
     }
 }
+//hashmap
+/**
+ * Definition for singly-linked list with a random pointer.
+ * class RandomListNode {
+ *     int label;
+ *     RandomListNode next, random;
+ *     RandomListNode(int x) { this.label = x; }
+ * };
+ */
+public class Solution {
+    /**
+     * @param head: The head of linked list with a random pointer.
+     * @return: A new head of a deep copy of the list.
+     */
+    public RandomListNode copyRandomList(RandomListNode head) {
+        if(head==null)
+            return head;
+        HashMap<RandomListNode,RandomListNode> map = new HashMap<RandomListNode,RandomListNode>();
+        RandomListNode p = head;
+        RandomListNode newhead = new RandomListNode(head.label);
+        RandomListNode q = newhead;
+        map.put(p,q);
+        p = p.next;
+        while(p!=null){
+            RandomListNode tmp = new RandomListNode(p.label);
+            map.put(p,tmp);
+            q.next = tmp;
+            q = tmp;
+            p = p.next;
+        }
+        p = head;
+        q = newhead;
+        while(p!=null){
+            if(p.random!=null){
+                q.random = map.get(p.random);
+            }else{
+                q.random = null;
+            }
+            p = p.next;
+            q = q.next;
+        }
+        return newhead;
+    }
+}
