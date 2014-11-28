d07aadf767aec6be41c21d2d09798e82c149632e
SingleNumber <yixiaomeng@gmail.com>
2014-11-28 14:55:04 -0800

    Update kthprimenumber.java

--- Leetcode/Lint/kthprimenumber.java
+++ Leetcode/Lint/kthprimenumber.java
@@ -4,6 +4,33 @@ The eligible numbers are like 3, 5, 7, 9, 15 ...
 
 Example
 If k=4, return 9.*/
+
+class Solution {
+    /**
+     * @param k: The number k.
+     * @return: The kth prime number as description.
+     */
+    public long kthPrimeNumber(int k) {
+    	int count3 = 0;
+        int count5 = 0;
+        int count7 = 0;
+
+        ArrayList<Long> list = new ArrayList<Long>();
+        list.add((long) 1);
+        long m = 0;
+        for(int i=1;i<=k;i++)
+        {
+            m = Math.min(Math.min(list.get(count3) * 3, list.get(count5) * 5), list.get(count7) * 7);
+
+            list.add(m);
+            if (m == list.get(count3) * 3) count3++;
+            if (m == list.get(count5) * 5) count5++;
+            if (m == list.get(count7) * 7) count7++;
+        }
+        return m;
+    }
+};
+
 //TLE
 class Solution {
     /**
