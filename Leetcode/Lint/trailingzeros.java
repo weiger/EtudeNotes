0abbe7760a3de58f0243e5ffa85c984bca63de82
SingleNumber <yixiaomeng@gmail.com>
2014-12-13 00:32:07 -0800

    Update trailingzeros.java

--- Leetcode/Lint/trailingzeros.java
+++ Leetcode/Lint/trailingzeros.java
@@ -3,6 +3,24 @@ Write an algorithm which computes the number of trailing zeros in n factorial.
 Example
 11! = 39916800, so the out should be 2
 */
+
+class Solution {
+    /*
+     * param n: As desciption
+     * return: An integer, denote the number of trailing zeros in n!
+     */
+    public long trailingZeros(long n) {
+        long res = 0;
+        while(n>0){
+            n/=5;
+            res+=n;
+        }
+        return res;
+    }
+};
+
+
+
 //TLE
 class Solution {
     /*
