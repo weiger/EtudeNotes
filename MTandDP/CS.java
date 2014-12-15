58b93d41b2e8a66736fed4a84898fc3ea3492484
SingleNumber <yixiaomeng@gmail.com>
2014-12-15 00:34:54 -0800

    Update CS.java

--- MTandDP/CS.java
+++ MTandDP/CS.java
@@ -112,3 +112,32 @@ class ServerService extends Thread{
 	}
 	
 }
+
+public class Solution {
+
+    public static void main(String[] args) {
+	ThreadB b = new ThreadB();
+	b.start();
+	synchronized(b){
+	    try {
+		b.wait();
+	    } catch (InterruptedException e) {
+		e.printStackTrace();
+	    }
+	}
+	System.out.println(b.total);
+    }
+}
+
+class ThreadB extends Thread {
+    int total;
+
+    public void run() {
+	synchronized(this){
+	    for (int i = 0; i < 100; i++) {
+		    total += i;
+		}
+	    notify();
+	}
+    }
+}
