121b3fa4e12bbc84ee6e072116513104d459d433
SingleNumber <yixiaomeng@gmail.com>
2014-12-24 16:00:17 -0800

    Update longestcommonsubsequence.java

--- Leetcode/Lint/longestcommonsubsequence.java
+++ Leetcode/Lint/longestcommonsubsequence.java
@@ -25,7 +25,7 @@ public class Solution {
         }
         for(int i = 1; i <= A.length(); i++){
             for(int j = 1; j <= B.length(); j++){
-                if(A.charAt(i-1) == B.charAt(j-1)){
+                if(A.charAt(i - 1) == B.charAt(j - 1)){
                     dp[i][j] = dp[i-1][j-1] + 1;
                 }else{
                     dp[i][j] = Math.max(dp[i - 1][j - 1],Math.max(dp[i - 1][j],dp[i][j - 1]));
