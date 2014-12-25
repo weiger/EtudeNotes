5fdcf896d3f653a70ccaa7c852995e0b10b2a341
SingleNumber <yixiaomeng@gmail.com>
2014-12-25 15:40:48 -0800

    Update mergeksortedlists.java

--- Leetcode/Lint/mergeksortedlists.java
+++ Leetcode/Lint/mergeksortedlists.java
@@ -53,3 +53,53 @@ public class Solution {
     }
 }
 
+/**
+ * Definition for ListNode.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int val) {
+ *         this.val = val;
+ *         this.next = null;
+ *     }
+ * }
+ */ 
+public class Solution {
+    /**
+     * @param lists: a list of ListNode
+     * @return: The head of one sorted list.
+     */
+    public ListNode mergeKLists(List<ListNode> lists) {  
+        if(lists == null || lists.size() == 0)
+            return null;
+        PriorityQueue<ListNode> pq = new PriorityQueue<ListNode>(lists.size(),new comp());
+        for(ListNode l:lists){
+            if(l != null)
+                pq.offer(l);
+        }
+        ListNode dummy = new ListNode(0);
+        ListNode cur = dummy;
+        while(pq.size() > 0){
+            ListNode next = pq.poll();
+            cur.next = next;
+            cur = cur.next;
+            if(cur.next != null){
+                pq.offer(cur.next);
+            }
+        }
+        return dummy.next;
+    }
+    class comp implements Comparator<ListNode>{
+        public int compare(ListNode p1,ListNode p2){
+            if(p1.val > p2.val){
+                return 1;
+            }else if(p1.val == p2.val){
+                return 0;
+            }else{
+                return -1;
+            }
+        }
+    }
+
+};
+
