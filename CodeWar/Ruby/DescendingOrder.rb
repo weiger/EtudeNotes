240c6020b3323bdc072a9f26915c1e7bc9bb1d95
SingleNumber <yixiaomeng@gmail.com>
2014-12-27 12:08:44 -0800

    Create DescendingOrder.rb

---
+++ CodeWar/Ruby/DescendingOrder.rb
@@ -0,0 +1,16 @@
+=begin
+Your task is to make a function that can take any non-negative integer as a argument and return it with it's digits in descending
+order. Descending order means that you take the highest digit and place the next highest digit immediately after it.
+
+Examples:
+
+Input: 145263
+Output: 654321
+
+Input: 1254859723
+Output: 9875543221
+=end
+
+def descending_order(n)
+  n.to_s.chars.sort.reverse.join.to_i
+end
