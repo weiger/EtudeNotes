a61f1fd22989b716da8eafaa0322f284497771ae
SingleNumber <yixiaomeng@gmail.com>
2014-12-27 11:49:07 -0800

    Update RemoveDuplicates.rb

--- CodeWar/Ruby/RemoveDuplicates.rb
+++ CodeWar/Ruby/RemoveDuplicates.rb
@@ -1,4 +1,4 @@
-=
+=begin
 Remove Duplicates
 
 You are to write a function called unique that takes an array of integers and returns the array with duplicates removed. It must return the values in the same order as first seen in the given array. Thus no sorting should be done, if 52 appears before 10 in the given array then it should also be that 52 appears before 10 in the returned array.
@@ -19,7 +19,7 @@ puts unique([]).inspect
 
 puts unique([5, 2, 1, 3]).inspect
 [5, 2, 1, 3]
-=
+=end
 
 def unique(integers)
   integers & integers
