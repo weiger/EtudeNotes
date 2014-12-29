1938d262675f8e35459fc56c936f74c0e668de8f
SingleNumber <yixiaomeng@gmail.com>
2014-12-29 09:59:37 -0800

    Update and rename RemoveDuplicatesfromSortedArrayII.java to removeduplicatesfromsortedarrayII.java

--- Leetcode/Lint/removeduplicatesfromsortedarrayII.java
+++ Leetcode/Lint/removeduplicatesfromsortedarrayII.java
@@ -30,3 +30,19 @@ public class Solution {
         return idx;
     }
 }
+
+public class Solution {
+    /**
+     * @param A: a array of integers
+     * @return : return an integer
+     */
+    public int removeDuplicates(int[] nums) {
+        int idx = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(idx < 2 || nums[idx - 2] != nums[i]){
+                nums[idx++] = nums[i];
+            }
+        }
+        return idx;
+    }
+}
