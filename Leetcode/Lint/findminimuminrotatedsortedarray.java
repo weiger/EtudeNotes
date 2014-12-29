682bdd892aa3419cb97a2edb425b085e299ba951
SingleNumber <yixiaomeng@gmail.com>
2014-12-29 15:23:26 -0800

    Create findminimuminrotatedsortedarray.java

---
+++ Leetcode/Lint/findminimuminrotatedsortedarray.java
@@ -0,0 +1,33 @@
+/*
+Suppose a sorted array is rotated at some pivot unknown to you beforehand.
+(i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+Find the minimum element.
+You may assume no duplicate exists in the array.
+Example
+Given [4,5,6,7,0,1,2] return 0
+*/
+public class Solution {
+    /**
+     * @param num: a rotated sorted array
+     * @return: the minimum number in the array
+     */
+    public int findMin(int[] num) {
+        if(num == null || num.length == 0)
+            return -1;
+        int beg = 0;
+        int end = num.length - 1;
+        while(beg < end){
+            int mid = beg + (end - beg)/2;
+            if(num[beg] < num[end]){
+                return num[beg];
+            }
+            if(num[beg] < num[mid]){
+                beg = mid;
+            }else if(num[beg] > num[mid]){
+                end = mid;
+            }else
+                beg++;
+        }
+        return num[beg];
+    }
+}
