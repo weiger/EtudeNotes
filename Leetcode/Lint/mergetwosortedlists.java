5ae4e60af876616a7a5705993d9b6c252cabbcfd
SingleNumber <yixiaomeng@gmail.com>
2014-12-30 14:21:01 -0800

    Create mergetwosortedlists.java

---
+++ Leetcode/Lint/mergetwosortedlists.java
@@ -0,0 +1,46 @@
+/*
+Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes 
+of the first two lists.
+Example
+Given 1->3->8->11->15->null, 2->null , return 1->2->3->8->11->15->null
+*/
+
+/**
+ * Definition for ListNode.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int val) {
+ *         this.val = val;
+ *         this.next = null;
+ *     }
+ * }
+ */ 
+public class Solution {
+    /**
+     * @param ListNode l1 is the head of the linked list
+     * @param ListNode l2 is the head of the linked list
+     * @return: ListNode head of linked list
+     */
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode cur = dummy;
+        while(l1 != null && l2 != null){
+            if(l1.val <= l2.val){
+                cur.next = l1;
+                l1 = l1.next;
+            }else{
+                cur.next = l2;
+                l2 = l2.next;
+            }
+            cur = cur.next;
+        }
+        if(l1 != null){
+            cur.next = l1;
+        }
+        if(l2 != null){
+            cur.next = l2;
+        }
+        return dummy.next;
+    }
+}
