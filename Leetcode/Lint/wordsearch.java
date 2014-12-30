1dc6de949f32e48ca2c1c74c6605c472059c4b9d
SingleNumber <yixiaomeng@gmail.com>
2014-12-30 15:37:29 -0800

    Update wordsearch.java

--- Leetcode/Lint/wordsearch.java
+++ Leetcode/Lint/wordsearch.java
@@ -18,22 +18,26 @@ public class Solution {
      * @return: A boolean
      */
     public boolean exist(char[][] board, String word) {
-        for(int i=0;i<board.length;i++){
-            for(int j=0;j<board[0].length;j++){
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j < board[0].length; j++){
                 if(helper(board,word,i,j))
                     return true;
             }
         }
         return false;
     }
+    
     public boolean helper(char[][] board,String word,int i,int j){
-        if(i<0 || j<0 || i>=board.length || j>=board[0].length || word.charAt(0)!=board[i][j])
+        if( i < 0 || j < 0 || i >= board.length || j >= board[0].length || word.charAt(0) != board[i][j])
             return false;
-        if(word.length()==1)
+        if( word.length() == 1)
             return true;
         board[i][j] = '#';
-        boolean f = helper(board,word.substring(1),i+1,j) || helper(board,word.substring(1),i,j+1) || helper(board,word.substring(1),i-1,j) || helper(board,word.substring(1),i,j-1) ;
+        if(helper(board,word.substring(1),i + 1,j) || helper(board,word.substring(1),i,j + 1) ||
+            helper(board,word.substring(1),i - 1,j) || helper(board,word.substring(1),i,j - 1))
+            return true;
         board[i][j] = word.charAt(0);
-        return f;
+        return false;
     }
 }
+
