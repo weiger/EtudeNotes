2447fe8a9f0ff77bd5771ca0f5e878638e397282
SingleNumber <yixiaomeng@gmail.com>
2015-01-08 17:23:11 -0800

    Update DivideTwoIntegers.java

--- Leetcode/DivideTwoIntegers.java
+++ Leetcode/DivideTwoIntegers.java
@@ -1,22 +1,26 @@
 /*
  * Divide two integers without using multiplication, division and mod operator.
  */
- O(logn)
-public class DivideTwoIntegers {
+//O(logn)
+public class Solution {
     public int divide(int dividend, int divisor) {
-        boolean flag = true;
-        if (dividend > 0 && divisor < 0 || dividend < 0 && divisor > 0)
-            flag = false;
-        int res = 0;
-        long a = Math.abs((long) dividend);
-        long b = Math.abs((long) divisor);
-        while (a >= b) {
-            int cnt = 0;
-            while (a >= (b << (cnt + 1)))
-                cnt++;
-            res += 1 << cnt;
-            a -= b << cnt;
-        }
-        return flag ? res : -res;
+    	if(dividend == Integer.MIN_VALUE && divisor == -1)
+    	    return Integer.MAX_VALUE;
+    	long p = Math.abs((long) dividend);
+    	long q = Math.abs((long) divisor);
+    	long ret = 0;
+    	while (p >= q) {
+    	    int counter = 0;
+    	    while (p >= (q << counter)) {
+    		counter++;
+    	    }
+    	    ret += 1 << (counter - 1);
+    	    p -= q << (counter - 1);
+    	}
+    	if ((dividend > 0 && divisor > 0) || (dividend < 0 && divisor < 0)) {
+    	    return (int) ret;
+    	} else {
+    	    return (int) (-ret);
+    	}
     }
 }
