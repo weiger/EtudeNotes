fb3febd98c59fd3718f65480b3878f9e79d4f9b7
SingleNumber <yixiaomeng@gmail.com>
2015-01-19 15:36:32 -0800

    Update kthlargestelement.java

--- Leetcode/Lint/kthlargestelement.java
+++ Leetcode/Lint/kthlargestelement.java
@@ -12,37 +12,38 @@ class Solution {
     //return: description of return
     public int kthLargestElement(int k, ArrayList<Integer> numbers) {
         if (k < 1 || k > numbers.size())
-	    return -1;
-    	int beg = 0;
-    	int end = numbers.size() - 1;
-    	while (beg < end) {
-    	    int j = partition(numbers, beg, end);
-    	    if (j < (k-1))
-    		    beg = j + 1;
-    	    else if (j>(k-1))
-    		    end = j - 1;
-    	    else
-    		    return numbers.get((k-1));
-    	}
-    	return numbers.get((k-1));
+        return -1;
+        int beg = 0;
+        int end = numbers.size() - 1;
+        while (beg < end) {
+            int j = partition(numbers, beg, end);
+            if (j < (k-1))
+                beg = j + 1;
+            else if (j>(k-1))
+                end = j - 1;
+            else
+                return numbers.get((k-1));
+        }
+        return numbers.get((k-1));
     }
     public int partition(ArrayList<Integer> numbers, int beg, int end) {
-    	if (beg > end)
-    	    return -1;
-    	int index = beg;
-    	int pivot = numbers.get(end);
-    	for (int i = beg; i < end; i++) {
-    	    if (numbers.get(i) >= pivot) {
-    		    swap(numbers, index++, i);
-    	        }
-    	    }
-    	    swap(numbers, index, end);
-    	    return index;
+        if (beg > end)
+            return -1;
+        int index = beg;
+        int pivot = numbers.get(end);
+        for (int i = beg; i < end; i++) {
+            if (numbers.get(i) >= pivot) {
+                swap(numbers, index++, i);
+            }
         }
+        swap(numbers, index, end);
+        return index;
+    }
     
     public void swap(ArrayList<Integer> numbers, int a, int b) {
-    	int tmp = numbers.get(a);
-    	numbers.set(a, numbers.get(b));
-    	numbers.set(b, tmp);
+        int tmp = numbers.get(a);
+        numbers.set(a, numbers.get(b));
+        numbers.set(b, tmp);
     }
 };
+
