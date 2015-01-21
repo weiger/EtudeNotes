a1d2f8b2a8838ce368732867b7f15c09de678644
SingleNumber <yixiaomeng@gmail.com>
2015-01-20 22:48:40 -0800

    Update Triangle.java

--- Leetcode/Triangle.java
+++ Leetcode/Triangle.java
@@ -25,6 +25,23 @@ from left to right !!!
  */
  public class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
+        if(triangle == null || triangle.size() == 0)
+            return 0;
+        int[] dp = new int[triangle.get(triangle.size() - 1).size()]; 
+        for(int i = 0; i < dp.length; i++){
+            dp[i] = triangle.get(triangle.size() - 1).get(i);
+        }
+        for(int i = triangle.size() - 2; i >=0 ; i--){
+            for(int j = 0; j < triangle.get(i).size(); j++){
+                dp[j] = triangle.get(i).get(j) + Math.min(dp[j],dp[j + 1]);
+            }
+        }
+        return dp[0];
+    }
+}
+
+ public class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
         int[] dp = new int[triangle.size()];
         for(int i=0;i<triangle.get(triangle.size()-1).size();i++){
             dp[i] = triangle.get(triangle.size()-1).get(i);
