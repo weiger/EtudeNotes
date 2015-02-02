3092e83ae923767905b288134dbda23eea8541e3
SingleNumber <yixiaomeng@gmail.com>
2015-02-01 20:52:09 -0800

    Update PermutationSequence.java

--- Leetcode/PermutationSequence.java
+++ Leetcode/PermutationSequence.java
@@ -16,9 +16,6 @@
  */
 
 public class PermutationSequence {
-
-/**************************** updated 2014.03.12 *****************************/
-
    public String getPermutation(int n, int k) {
         String res = "";
         String s = "";
@@ -38,6 +35,28 @@ public class PermutationSequence {
         return res;
     } 
 }
+public class Solution {
+    public String getPermutation(int n, int k) {
+        StringBuilder result = new StringBuilder();
+        List<Integer> nums = new ArrayList<Integer>();
+        for(int i = 1; i <= n; ++i) 
+            nums.add(i);
+        int factor = 1;
+        for(int i = 1; i <= n; ++i) 
+            factor *= i;
+        factor /= n;
+        for(int i = 1; i <= n; ++i) {
+            result.append(nums.get((k - 1) / factor) + "");
+            nums.remove((k - 1) / factor);
+            k = (k - 1) % factor + 1;
+            if(i != n) 
+                factor /= n - i;
+        }
+        return result.toString();
+    }
+}
+
+
 /************************************************************************/
 //Time Limit Exceeded
 public class Solution {
