c730b9b2cf33136e79429e4c966e13a67c5db1d8
SingleNumber <yixiaomeng@gmail.com>
2015-02-08 22:22:14 -0800

    Update Pow.java

--- Leetcode/Pow.java
+++ Leetcode/Pow.java
@@ -1,20 +1,6 @@
 /*
  * Implement pow(x, n).
  */
-
-public class Pow {
-    public double pow(double x, int n) {
-        if(n == 0)
-            return 1;
-        double half = pow(x, n / 2);
-        if(n % 2 == 0)
-            return half * half;
-        else if(n > 0)
-            return half * half * x;
-        else 
-            return half * half / x;
-    }
-}
 public class Solution {
     public double pow(double x, int n) {
         if(n<0) return 1/helper(x,-n);
