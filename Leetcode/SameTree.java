82f5a5d06dd9197b55943066347715780ac79079
SingleNumber <yixiaomeng@gmail.com>
2015-03-06 13:40:29 -0800

    Update SameTree.java

--- Leetcode/SameTree.java
+++ Leetcode/SameTree.java
@@ -32,3 +32,31 @@ public class SameTree {
         return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
 }
+
+public class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        Queue<TreeNode> queue1 = new LinkedList<TreeNode>();
+        Queue<TreeNode> queue2 = new LinkedList<TreeNode>();
+        queue1.offer(p);
+        queue2.offer(q);
+        while (!queue1.isEmpty() && !queue2.isEmpty()) {
+            TreeNode q1 = queue1.poll();
+            TreeNode q2 = queue2.poll();
+            if (q1 == null && q2 == null) {
+                continue;
+            } else if (q1 == null || q2 == null) {
+                return false;
+            } else if (q1.val != q2.val) {
+                return false;
+            }
+            queue1.offer(q1.left);
+            queue2.offer(q2.left);
+            queue1.offer(q1.right);
+            queue2.offer(q2.right);
+        }
+        if (!queue1.isEmpty() || !queue2.isEmpty()) {
+            return false;
+        }
+        return true;
+    }
+}
