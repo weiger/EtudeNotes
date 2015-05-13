088168501274fc96dd9fd730abf7a841c1ac8a65
SingleNumber <yixiaomeng@gmail.com>
2015-05-12 23:18:22 -0700

    Create Substitute.java

---
+++ topcoder/Substitute.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+
+public class topcoder {
+	public int getValue(String key, String code){
+		HashMap<Character,Integer> map = new HashMap<Character, Integer>();
+		for(int i = 0 ; i < key.length(); i++){
+			map.put(key.charAt(i), (i + 1)% 10);
+		}
+		int res = 0;
+		for(int i = 0 ; i < code.length(); i++){
+			if(!map.containsKey(code.charAt(i))){
+				continue;
+			}
+			res = res * 10 + map.get(code.charAt(i));
+		}
+		return res;	
+	}
+}
