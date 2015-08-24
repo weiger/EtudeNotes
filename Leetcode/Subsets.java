779d1bf073dcb0bbb3774450b5cdab0e1797744c
Xiaomeng Yi <yixiaomeng@gmail.com>
2015-08-24 12:00:11 -0700

    Update Subsets.java

--- Leetcode/Subsets.java
+++ Leetcode/Subsets.java
@@ -20,64 +20,20 @@
  * ]
  */
 
-import java.util.*;
-
-public class Subsets {
-    public static String convertInt2BinStr(int n, int len) {
-        String s = Integer.toBinaryString(n);
-        while(s.length() < len) {
-            s = "0" + s;
-        }
-        return s;
-    }
-
-    public ArrayList<ArrayList<Integer>> subsets(int[] S) {
-        ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();
-        int len = S.length;
-        Arrays.sort(S);
-        for(int i = 0; i < (1 << len); i++) {
-            String s = convertInt2BinStr(i, len);
-            ArrayList<Integer> list = new ArrayList<Integer>();
-            for(int j = 0; j < len; j++) {
-                if(s.charAt(j) == '1')
-                    list.add(S[j]);
-            }
-            res.add(list);
-        }
+public class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
+        List<Integer> list = new ArrayList<>();
+        helper(res, list, nums, 0);
         return res;
     }
-    
-    public ArrayList<ArrayList<Integer>> subsets(int[] S) {
-        ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();
-        Arrays.sort(S);
-        res.add(new ArrayList<Integer>());
-        for(int i = 0; i < S.length; i++) {
-            int k = res.size();
-            for(int j = 0; j < k; j++) {
-                ArrayList<Integer> list = new ArrayList<Integer>(res.get(j));
-                list.add(S[i]);
-                res.add(list);
-            }
-        }
-        return res;
-    }
-
-    public void dfs(int[] S, int pos, ArrayList<ArrayList<Integer>> res, 
-            ArrayList<Integer> list) {
-        for(int i = pos; i < S.length; i++) {
-            list.add(S[i]);
-            res.add(new ArrayList<Integer>(list));
-            dfs(S, i + 1, res, list);
+    public void helper(List<List<Integer>> res, List<Integer> list, int[] nums, int pos){
+        res.add(new ArrayList<>(list));
+        for(int i = pos; i < nums.length; i++){
+            list.add(nums[i]);
+            helper(res, list, nums, i + 1);
             list.remove(list.size() - 1);
         }
-    }
-
-    public ArrayList<ArrayList<Integer>> subsets(int[] S) {
-        ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();
-        Arrays.sort(S);
-        ArrayList<Integer> list = new ArrayList<Integer>();
-        res.add(list);
-        dfs(S, 0, res, list);
-        return res;
     }
 }
