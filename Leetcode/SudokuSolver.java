e47315d53c63ad97963e367103603a49248bb6ed
Xiaomeng Yi <yixiaomeng@gmail.com>
2015-08-24 19:10:49 -0700

    Update SudokuSolver.java

--- Leetcode/SudokuSolver.java
+++ Leetcode/SudokuSolver.java
@@ -6,21 +6,19 @@ You may assume that there will be only one unique solution. */
 
 public class Solution {
     public void solveSudoku(char[][] board) {
-        if(board==null || board.length==0)
-            return;
         solve(board);
     }
     public boolean solve(char[][] board){
-        for(int i=0;i<board.length;i++){
-            for(int j=0;j<board[0].length;j++){
-                if(board[i][j]=='.'){
-                    for(char m='1';m<='9';m++){
-                        if(isvalid(board,i,j,m)){
-                            board[i][j] = m;
+        for(int i = 0 ; i < board.length; i++){
+            for(int j = 0 ; j < board[0].length; j++){
+                if(board[i][j] == '.'){
+                    for(char c = '1'; c <= '9'; c++){
+                        if(isValid(board, i, j, c)){
+                            board[i][j] = c;
                             if(solve(board))
                                 return true;
                             else
-                                board[i][j]='.';
+                                board[i][j] = '.';
                         }
                     }
                     return false;
@@ -29,21 +27,23 @@ public class Solution {
         }
         return true;
     }
-    public boolean isvalid(char[][] board,int i,int j,char m){
-        for(int row=0;row<9;row++){
-            if(board[row][j]==m)
+    public boolean isValid(char[][] board, int i, int j, char c) {
+        for(int row = 0 ; row < 9; row++){
+            if(board[row][j] == c)
                 return false;
         }
-        for(int col=0;col<9;col++){
-            if(board[i][col]==m)
+        for(int col = 0; col < 9; col++){
+            if(board[i][col] == c)
                 return false;
         }
-        for(int row=i/3*3;row<i/3*3+3;row++){
-            for(int col=j/3*3;col<j/3*3+3;col++){
-                if(board[row][col]==m)
+        for(int row = i / 3 * 3; row < i / 3 * 3 + 3; row++){
+            for(int col = j / 3 * 3; col < j / 3 * 3 + 3; col++){
+                if(board[row][col] == c){
                     return false;
+                }
             }
         }
         return true;
     }
+
 }
