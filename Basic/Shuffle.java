f7fafddede02f3460dc6f679f092e30986783c34
Xiaomeng Yi <yixiaomeng@gmail.com>
2015-08-26 22:48:28 -0700

    Update Shuffle.java

--- Basic/Shuffle.java
+++ Basic/Shuffle.java
@@ -1,7 +1,8 @@
 public void shuffle(int[] A){
 	for(int i = 0;i<A.length;i++){
 	    int j = (int)(Math.random() * (A.length-i)) + i;
-	    A[j] = i;
-	    A[i] = j;
+	    int tmp = A[j];
+	    A[j] = A[i];
+	    A[i] = tmp;
 	 }
 }
