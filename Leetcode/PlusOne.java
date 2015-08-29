662d4704c958d85914a54e2ddf871ef0b3804f29
Xiaomeng Yi <yixiaomeng@gmail.com>
2015-08-28 22:45:52 -0700

    Update PlusOne.java

--- Leetcode/PlusOne.java
+++ Leetcode/PlusOne.java
@@ -1,38 +1,19 @@
 /*
  * Given a number represented as an array of digits, plus one to the number.
  */
-public class PlusOne {
-
+public class Solution {
     public int[] plusOne(int[] digits) {
-        int len = digits.length;
-        int carrier = 1;
-        int i = len - 1;
-        while(carrier == 1 && i >= 0) {
-            digits[i] += carrier;
-            carrier = digits[i] / 10;
-            digits[i] %= 10;
-            i--;
+        int carry = 1;
+        for(int i = digits.length - 1; i >= 0; i--){
+            int tmp = digits[i];
+            digits[i] = (digits[i] + carry) % 10;
+            carry = (tmp + carry) / 10;
         }
-        if(carrier == 1) {
-            digits = new int[len + 1];
-            digits[0] = 1;
-        }
-        return digits;
-    }
-
-/*****************************************************************************/
-
-   public int[] plusOne(int[] digits) {
-        int len = digits.length;
-        int index = len - 1;
-        while(index >= 0 && digits[index] == 9) {
-            digits[index--] = 0;
-        }
-        if(index >= 0)
-            digits[index]++;
-        else {
-            digits = new int[len + 1];
-            digits[0] = 1;
+        if(carry == 1){
+            int[] res = new int[digits.length + 1];
+            Arrays.fill(res, 0);
+            res[0] = 1;
+            return res;
         }
         return digits;
     }
