922734cd284e01e57ca6782f821cbc74f3f354a5
Xiaomeng Yi <yixiaomeng@gmail.com>
2015-08-30 14:24:52 -0700

    Update InterleavingString.java

--- Leetcode/InterleavingString.java
+++ Leetcode/InterleavingString.java
@@ -1,54 +1,27 @@
-/*
- * Given s1, s2, s3, find whether s3 is formed by the interleaving of s1 and s2.
-
- * For example,
- * Given:
- * s1 = "aabcc",
- * s2 = "dbbca",
-
- * When s3 = "aadbbcbcac", return true.
- * When s3 = "aadbbbaccc", return false.
- */
- 
-public boolean isInterleave(String s1, String s2, String s3) {
-    if(s1.length()+s2.length()!=s3.length())
-        return false;
-    String minWord = s1.length()>s2.length()?s2:s1;
-    String maxWord = s1.length()>s2.length()?s1:s2;
-    boolean[] res = new boolean[minWord.length()+1];
-    res[0] = true;
-    for(int i=0;i<minWord.length();i++)
-    {
-        res[i+1] = res[i] && minWord.charAt(i)==s3.charAt(i);
-    }
-    for(int i=0;i<maxWord.length();i++)
-    {
-        res[0] = res[0] && maxWord.charAt(i)==s3.charAt(i);
-        for(int j=0;j<minWord.length();j++)
-        {
-            res[j+1] = res[j+1]&&maxWord.charAt(i)==s3.charAt(i+j+1) || res[j]&&minWord.charAt(j)==s3.charAt(i+j+1);
-        }
-    }
-    return res[minWord.length()];
-}
- 
-//dp[i][j] means string s1 with length i and string s2 with length j can be interleave to i+j length string
 public class Solution {
     public boolean isInterleave(String s1, String s2, String s3) {
-        if(s1.length() + s2.length() != s3.length())
+        if(s1 == null || s2 == null || s3 == null){
             return false;
-        boolean[][] dp = new boolean[s1.length() + 1][s2.length() + 1];
+        }
+        if(s1.length() + s2.length() != s3.length()){
+            return false;
+        }
+        int M = s1.length(); int N = s2.length();
+        boolean[][] dp = new boolean[M + 1][N + 1];
         dp[0][0] = true;
-        for(int i = 1; i <= s1.length(); i++) 
-            dp[i][0] = dp[i - 1][0] && (s1.charAt(i - 1) == s3.charAt(i - 1));
-        for(int i = 1; i <= s2.length(); i++)
-            dp[0][i] = dp[0][i - 1] && (s2.charAt(i - 1) == s3.charAt(i - 1));
-        for(int i = 1; i <= s1.length(); i++) {
-            for(int j = 1; j <= s2.length(); j++) {
-                dp[i][j] = dp[i - 1][j] && (s1.charAt(i - 1) == s3.charAt(i + j - 1)) 
-                    || dp[i][j - 1] && (s2.charAt(j - 1) == s3.charAt(i + j - 1));
+        for(int i = 1; i <= M; i++){
+            dp[i][0] = dp[i - 1][0] && s1.charAt(i - 1) == s3.charAt(i - 1);
+        }
+        for(int j = 1; j <= N; j++){
+            dp[0][j] = dp[0][j - 1] && s2.charAt(j - 1) == s3.charAt(j - 1);
+        }
+        for(int i = 1; i <= M; i++){
+            for(int j = 1; j <= N; j++){
+                boolean b1 = dp[i - 1][j] && s1.charAt(i - 1) == s3.charAt(i + j - 1); 
+                boolean b2 = dp[i][j - 1] && s2.charAt(j - 1) == s3.charAt(i + j - 1);
+                if(b1 || b2) dp[i][j] = true;
             }
         }
-        return dp[s1.length()][s2.length()];
+        return dp[M][N];
     }
 }
