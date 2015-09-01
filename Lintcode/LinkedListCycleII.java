e48053c50aff2ecdcadc75eaee4505af23c8ae20
Xiaomeng Yi <yixiaomeng@gmail.com>
2015-09-01 11:05:41 -0700

    Create LinkedListCycleII.java

---
+++ Lintcode/LinkedListCycleII.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for ListNode.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int val) {
+ *         this.val = val;
+ *         this.next = null;
+ *     }
+ * }
+ */ 
+public class Solution {
+    /**
+     * @param head: The first node of linked list.
+     * @return: The node where the cycle begins. 
+     *           if there is no cycle, return null
+     */
+    public ListNode detectCycle(ListNode head) {  
+        // write your code here
+        if(head == null || head.next == null){
+            return null;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast){
+                break;
+            }
+        }
+        if(fast == null || fast != slow){
+            return null;
+        }
+        fast = head;
+        while(true){
+            if(slow == fast){
+                return slow;
+            } 
+            slow = slow.next;
+            fast = fast.next;
+        }
+    }
+}
