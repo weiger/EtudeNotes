dbe7e8a8c6061ba93bfbc4f41d255249c5153fc8
Xiaomeng Yi <yixiaomeng@gmail.com>
2015-09-01 02:40:45 -0700

    Create BalancedBinaryTree.java

---
+++ Lintcode/BalancedBinaryTree.java
@@ -0,0 +1,32 @@
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+public class Solution {
+    /**
+     * @param root: The root of binary tree.
+     * @return: True if this Binary tree is Balanced, or false.
+     */
+    public boolean isBalanced(TreeNode root) {
+        // write your code here
+        if(root == null){
+            return true;
+        }
+        int left = height(root.left);
+        int right = height(root.right);
+        return Math.abs(left - right) <= 1 && isBalanced(root.left) && isBalanced(root.right);
+    }
+    public int height(TreeNode p){
+        if(p == null){
+            return 0;
+        }
+        return 1 + Math.max(height(p.left), height(p.right));
+    }
+}
