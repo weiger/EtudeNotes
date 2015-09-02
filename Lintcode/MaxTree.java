4f32ef16ab79bbcb45c1d1d67e328ebe3e0b0909
Xiaomeng Yi <yixiaomeng@gmail.com>
2015-09-01 22:03:54 -0700

    Update and rename MaxTree,java to MaxTree.java

--- Lintcode/MaxTree.java
+++ Lintcode/MaxTree.java
@@ -1,15 +1,4 @@
 /* 什么鬼！
-/**
- * Definition of TreeNode:
- * public class TreeNode {
- *     public int val;
- *     public TreeNode left, right;
- *     public TreeNode(int val) {
- *         this.val = val;
- *         this.left = this.right = null;
- *     }
- * }
- */
 public class Solution {
     /**
      * @param A: Given an integer array with no duplicates.
