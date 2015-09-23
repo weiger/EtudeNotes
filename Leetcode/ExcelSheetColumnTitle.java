b46be11da622bf4ae3949e2867c0044ae376416a
Xiaomeng Yi <yixiaomeng@gmail.com>
2015-09-22 21:05:42 -0700

    Create ExcelSheetColumnTitle.java

---
+++ Leetcode/ExcelSheetColumnTitle.java
@@ -0,0 +1,11 @@
+public class Solution {
+    public String convertToTitle(int n) {
+        StringBuilder res = new StringBuilder();
+        while (n != 0) {
+            n--;
+            res.append(char(n % 26 + 'A'));
+            n /= 26;
+        }
+        return res.reverse().toString();
+    }
+}
