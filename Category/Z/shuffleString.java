49ef1f61f8d77e5eb5f413c44ae81d639ff56b70
Xiaomeng Yi <yixiaomeng@gmail.com>
2015-09-24 11:35:06 -0700

    Update shuffleString.java

--- Category/Z/shuffleString.java
+++ Category/Z/shuffleString.java
@@ -8,17 +8,17 @@ public class Solution {
         int prev = -1;
         for(int i = 0 ; i < arr.length; i++){
             if(arr[i] == ' '){
-                helper(arr, prev + 1, i - 1);
+                helper(arr, prev + 2, i - 1);
                 prev = i;
             }
             if(i == arr.length - 1){
-                helper(arr, prev + 1, arr.length - 1);
+                helper(arr, prev + 2, arr.length - 1);
             }
         }
         return new String(arr);
     }
     public static void helper(char[] arr, int beg ,int end){
-        for(int i = beg + 1 ; i <= end - 1 ; i++){
+        for(int i = beg  ; i < end; i++){
             int j = (int)(Math.random() * (end - i)) + i;
             char tmp = arr[j];
             arr[j] = arr[i];
@@ -26,7 +26,8 @@ public class Solution {
         }
     }
     public static void main(String[] args) {
-        System.out.println(new Solution().shuffleStr("I want to buy a cup of water"));
+        for (int i = 0 ; i < 10; i++)
+            System.out.println(new Solution().shuffleStr("I want to buy a cup of water"));
     }
 }
 
