b95beb5bf5d6ae8e257885b367c163c377f9da54
Xiaomeng Yi <yixiaomeng@gmail.com>
2015-09-24 21:35:17 -0700

    Update 3sumless.java

--- Category/Z/3sumless.java
+++ Category/Z/3sumless.java
@@ -1,16 +1,21 @@
 public class Solution {
     public int threesumless(int[] nums, int target) {
+        if(nums == null || nums.length == 0){
+            return 0;
+        }
+        Arrays.sort(nums);
         int cnt = 0;
-        for(int i = 0; i < nums.length - 1; i++){
-            int beg = i + 1;
-            int end = nums.length - 1;
-            while(beg < end){
-                int sum = nums[i] + nums[beg] + nums[end];
-                if(sum <= target){
-                    cnt += end - beg;
-                    beg++;
-                }else{
-                    end--;
+        for(int i = 0 ; i < nums.length; i++){
+            for(int j = i + 1; j < nums.length; j++){
+                int beg = j; int end = nums.length - 1;
+                while(beg < end){
+                    int sum = nums[i] + nums[beg] + nums[end];
+                    if(sum >= target){
+                        end--;
+                    }else{
+                        cnt += end - beg;
+                        break;
+                    }
                 }
             }
         }
