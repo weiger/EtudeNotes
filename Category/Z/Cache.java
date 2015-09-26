31c189b72c018a5e747f72f0aac51853772b06d6
Xiaomeng Yi <yixiaomeng@gmail.com>
2015-09-25 20:44:17 -0700

    Create Cache.java

---
+++ Category/Z/Cache.java
@@ -0,0 +1,25 @@
+public Cache{
+	HashMap<Integer, Integer> map;
+	int num;
+
+	public Cache () {
+		map = new HashMap<>();
+		num = 1;
+	}
+
+	public void set(int x) {
+		map.put(x, num);
+	}
+
+	public void reset(int x) {
+		map.remove(x);
+	}
+
+	public boolean exists(int x) {
+		return map.get(x) != null && map.get(x) == num;
+	}
+
+	public void resetAll(){
+		num++;
+	}
+}
