0ea3c13b6a403e945b220c93bed7f2cf2b9507ca
Xiaomeng Yi <yixiaomeng@gmail.com>
2015-10-02 19:05:37 -0700

    Update PalindromePermutationII.java

--- Leetcode/PalindromePermutationII.java
+++ Leetcode/PalindromePermutationII.java
@@ -8,6 +8,52 @@ Given s = "abc", return [].
 public class Solution {
     public List<String> generatePalindromes(String s) {
         List<String> res = new ArrayList<>();
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (int i = 0 ; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (!map.containsKey(c)) {
+                map.put(c, 1);
+            } else {
+                map.put(c, map.get(c) + 1);
+            }
+        }
+        int cnt = 0;
+        char odd = 0;
+        for (Character c: map.keySet()) {
+            if (map.get(c) % 2 == 1) {
+                cnt++;
+                odd = c;
+            }
+            if (cnt > 1) {
+                return res;
+            }
+        }
+        String buffer = "";
+        if (cnt == 1) {
+            map.put(odd, map.get(odd) - 1);
+            buffer += odd;
+        }
+        dfs(res, s, map, buffer);
+        return res;
+    }
+    public void dfs(List<String> res, String s, HashMap<Character, Integer> map, String buffer){
+        if (buffer.length() == s.length()) {
+            res.add(buffer);
+            return;
+        }
+        for (char c: map.keySet()) {
+            if (map.get(c) > 0) {
+                map.put(c, map.get(c) - 2);
+                dfs(res, s, map, c + buffer + c);
+                map.put(c, map.get(c) + 2);
+            }
+        }
+    }
+}
+
+public class Solution {
+    public List<String> generatePalindromes(String s) {
+        List<String> res = new ArrayList<>();
         int[] map = new int[256];
         boolean odd = false;
         char single = 0;
