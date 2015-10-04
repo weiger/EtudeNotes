9228773a4c1e345f0d6ce4a8cf42819afd2fcc4f
Xiaomeng Yi <yixiaomeng@gmail.com>
2015-10-04 12:34:14 -0700

    Update GenerateParentheses.java

--- Leetcode/GenerateParentheses.java
+++ Leetcode/GenerateParentheses.java
@@ -24,20 +24,27 @@ public class Solution {
     }
 } 
  
- 
-public class GenerateParentheses {
-    public ArrayList<String> generateParenthesis(int n) {
-        ArrayList<String> res = new ArrayList<String>();
-        if(n == 0) 
-            res.add("");
-        else if(n == 1) 
-            res.add("()");
-        else {
-            for(int i = 0; i < n; i++) 
-                for(String inner : generateParenthesis(i)) 
-                    for(String outer : generateParenthesis(n - 1 - i))
-                        res.add("(" + inner + ")" + outer);
+public class Solution {
+    public static List<String> generateParenthesis(int n) {
+        List<List<String>> res = new ArrayList<>();
+        List<String> one = new ArrayList<>();
+        one.add("");
+        res.add(one);
+
+        for (int i = 1; i <= n; i++) {
+            List<String> list = new ArrayList<>();
+            for (int j = 0; j < i; j++) {
+                List<String> left = res.get(j);
+                List<String> right = res.get(i - j - 1);
+                for (int k = 0; k < left.size(); k++) {
+                    for (int l = 0; l < right.size(); l++) {
+                        list.add("(" + left.get(k) + ")" + right.get(l));
+                    }
+                }
+            }
+            res.add(list);
         }
-        return res;
+        return res.get(n);
     }
 }
+
