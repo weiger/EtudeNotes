592e8035c648906999397a0670e244c4f1aa0ee4
Xiaomeng Yi <yixiaomeng@gmail.com>
2015-10-11 21:53:37 -0700

    Update Anagrams.java

--- Leetcode/Anagrams.java
+++ Leetcode/Anagrams.java
@@ -4,31 +4,30 @@
  * Note: All inputs will be in lower-case.
  * For example:
 
- * Input:¡¡¡¡["tea","and","ate","eat","den"]
+ * Input:Â¡Â¡Â¡Â¡["tea","and","ate","eat","den"]
 
  * Output:   ["tea","ate","eat"]
  * 
  */
-public class Anagrams {
-
-    public ArrayList<String> anagrams(String[] strs) {
-        ArrayList<String> res = new ArrayList<String>();
-        Map<String, Integer> map = new HashMap<String, Integer>();
+public class Solution {
+    
+    public List<String> anagrams(String[] strs) {
+        List<String> res = new ArrayList<>();
+        HashMap<String, Integer> map = new HashMap<>();
         for (int i = 0; i < strs.length; i++) {
-            char[] c = strs[i].toCharArray();
-            Arrays.sort(c);
-            String s = new String(c);
-            if (!map.containsKey(s)) {
-                map.put(s, i);
+            char[] arr = strs[i].toCharArray();
+            Arrays.sort(arr);
+            String newstr = new String(arr);
+            if (!map.containsKey(newstr)) {
+                map.put(newstr, i);
             } else {
-                if (map.get(s) >= 0) {
-                    res.add(strs[map.get(s)]);
-                    map.put(s, -1);
+                if (map.get(newstr) >= 0) {
+                    res.add(strs[map.get(newstr)]);
+                    map.put(newstr, -1);
                 }
                 res.add(strs[i]);
             }
         }
         return res;
     }
-
 }
