ef4f4ea8013b5d43a3e58b332820d30a115de355
Xiaomeng Yi <yixiaomeng@gmail.com>
2015-10-25 14:09:40 -0700

    Update ReverseLinkedList.java

--- Leetcode/ReverseLinkedList.java
+++ Leetcode/ReverseLinkedList.java
@@ -7,6 +7,39 @@
  * }
  */
 public class Solution {
+    public ListNode reverseList(ListNode head) {
+        if(head == null || head.next == null)
+            return head;
+        ListNode p = head.next;
+        head.next = null;
+        ListNode q = null;
+        while(p != null){
+            q = p.next;
+            p.next = head;
+            head = p;
+            p = q;
+        }
+        return head;
+    }
+}
+
+public class Solution {
+    public ListNode reverseList(ListNode head) {
+        if(head == null || head.next == null)
+            return head;
+        ListNode next = head.next;
+        head.next = null;
+        ListNode newhead = reverseList(next);
+        ListNode cur = newhead;
+        while(cur.next != null){
+            cur = cur.next;
+        }
+        cur.next = head;
+        return newhead;
+    }
+} 
+ 
+public class Solution {
     ListNode left;
     boolean meet;
     public ListNode reverseList(ListNode head) {
