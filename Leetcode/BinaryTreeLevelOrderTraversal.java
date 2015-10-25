513edd012caab9abc6f8e266bcea241fc7678697
Xiaomeng Yi <yixiaomeng@gmail.com>
2015-10-25 13:35:28 -0700

    Update BinaryTreeLevelOrderTraversal.java

--- Leetcode/BinaryTreeLevelOrderTraversal.java
+++ Leetcode/BinaryTreeLevelOrderTraversal.java
@@ -69,3 +69,29 @@ public class Solution {
         dfs(p.right, res, level + 1);
     }
 }
+
+public class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) return res;
+        Queue<TreeNode> q = new LinkedList<>();
+        List<Integer> list = new ArrayList<>();
+        q.offer(root);
+        while (!q.isEmpty()){
+            int size = q.size();
+            for (int i = 0 ; i < size; i++) {
+                TreeNode cur = q.poll();
+                list.add(cur.val);
+                if (cur.left != null) {
+                    q.offer(cur.left);
+                }
+                if (cur.right != null) {
+                    q.offer(cur.right);
+                }
+            }
+            res.add(list);
+            list = new ArrayList<>();
+        }
+        return res;
+    } 
+}
