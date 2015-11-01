f792e7ff9a75598f03a580e267fb10ac2f8a9c30
Xiaomeng Yi <yixiaomeng@gmail.com>
2015-10-31 22:07:11 -0700

    Update ZigZagConversion.java

--- Leetcode/ZigZagConversion.java
+++ Leetcode/ZigZagConversion.java
@@ -1,32 +1,29 @@
 /*
- * The string "PAYPALISHIRING" is written in a zigzag pattern on a given number 
- * of rows like this: (you may want to display this pattern in a fixed font for 
- * better legibility)
+P   | A   | H   | N
+A P | L S | I I | G
+Y   | I   | R   |
 
- * P   A   H   N
- * A P L S I I G
- * Y   I   R
- * And then read line by line: "PAHNAPLSIIGYIR"
- * Write the code that will take a string and make this conversion given a 
- * number of rows:
+cycle = (2*nRows - 2), except nRows == 1.
+j = i + cycle*k, k = 0, 1, 2, ...
+secondJ = (j - i) + cycle - i
 
- * string convert(string text, int nRows);
- * convert("PAYPALISHIRING", 3) should return "PAHNAPLSIIGYIR". 
- */
-public class ZigZagConversion {
-    public String convert(String s, int nRows) {
-        if (nRows == 1)
+*/
+public class Solution {
+    public String convert(String s, int numRows) {
+        if (numRows <= 1) {
             return s;
-        String res = "";
-        int len = s.length();
-        int T = 2 * nRows - 2;
-        for (int i = 0; i < nRows; i++) {
-            for (int j = 0; i + j * T < len; j++) {
-                res += s.charAt(i + j * T);
-                if (i > 0 && i < nRows - 1 && (T - i + j * T < len))
-                    res += s.charAt(T - i + j * T);
+        }
+        StringBuilder sb = new StringBuilder();
+        int cycle = 2 * numRows - 2;
+        for (int i = 0 ; i < numRows; i++) {
+            for (int j = i; j < s.length(); j = j + cycle) {
+                sb.append(s.charAt(j));
+                int nextIndex = (j - i) + cycle - i;
+                if (i != 0 && i != numRows - 1 && nextIndex < s.length()) {
+                    sb.append(s.charAt(nextIndex));
+                }
             }
         }
-        return res;
+        return sb.toString();
     }
 }
