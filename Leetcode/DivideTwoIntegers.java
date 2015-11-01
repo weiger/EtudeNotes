85a6b89d170ee96d216f8d2780568ed8853f1d11
Xiaomeng Yi <yixiaomeng@gmail.com>
2015-10-31 23:40:43 -0700

    Update DivideTwoIntegers.java

--- Leetcode/DivideTwoIntegers.java
+++ Leetcode/DivideTwoIntegers.java
@@ -1,26 +1,24 @@
-/*
- * Divide two integers without using multiplication, division and mod operator.
- */
-//O(logn)
 public class Solution {
     public int divide(int dividend, int divisor) {
-    	if(dividend == Integer.MIN_VALUE && divisor == -1)
-    	    return Integer.MAX_VALUE;
-    	long p = Math.abs((long) dividend);
-    	long q = Math.abs((long) divisor);
-    	long ret = 0;
-    	while (p >= q) {
-    	    int counter = 0;
-    	    while (p >= (q << counter)) {
-    		counter++;
-    	    }
-    	    ret += 1 << (counter - 1);
-    	    p -= q << (counter - 1);
-    	}
-    	if ((dividend > 0 && divisor > 0) || (dividend < 0 && divisor < 0)) {
-    	    return (int) ret;
-    	} else {
-    	    return (int) (-ret);
-    	}
+        long res = helper(dividend, divisor);
+        return res > Integer.MAX_VALUE ? Integer.MAX_VALUE : (int) res;
+    }
+
+    public long helper(long dividend, long divisor) {
+        boolean neg = dividend < 0 != divisor < 0;
+        if (dividend < 0) dividend = -dividend;
+        if (divisor < 0) divisor = -divisor;
+        if (dividend < divisor) return 0;
+        long sum = divisor;
+        long divide = 1;
+        while ((sum + sum) <= dividend) {
+            sum += sum;
+            divide += divide;
+        }
+        if (neg) {
+            return -(divide + helper((dividend - sum), divisor));
+        } else {
+            return divide + helper((dividend - sum), divisor);
+        }
     }
 }
