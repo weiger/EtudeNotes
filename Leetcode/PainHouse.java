890842d63127d0570d5ec9ea92ec86eef2567d51
Xiaomeng Yi <yixiaomeng@gmail.com>
2015-11-01 11:50:11 -0800

    Update PainHouse.java

--- Leetcode/PainHouse.java
+++ Leetcode/PainHouse.java
@@ -1,19 +1,28 @@
 public class Solution {
     public int minCost(int[][] costs) {
-        if(costs == null || costs.length == 0){
+        if(costs == null || costs.length == 0)
             return 0;
+        int a = costs[0][0];
+        int b = costs[0][1];
+        int c = costs[0][2];
+        for (int i = 1; i < costs.length; i++) {
+            int olda = a; int oldb = b;
+            a = costs[i][0] + Math.min(b, c);
+            b = costs[i][1] + Math.min(olda, c);
+            c = costs[i][2] + Math.min(olda, oldb);
         }
-        int red = costs[0][0];
-        int blue = costs[0][1];
-        int green = costs[0][2];
-        for(int i = 1; i < costs.length; i++){
-            int oldred = red;
-            int oldgreen = green;
-            int oldblue = blue;
-            red = Math.min(green, blue) + costs[i][0];
-            blue = Math.min(oldred, oldgreen) + costs[i][1];
-            green = Math.min(oldred, oldblue) + costs[i][2];
+        return Math.min(a, Math.min(b, c));
+    }
+}
+public class Solution {
+    public int minCost(int[][] costs) {
+        if(costs == null || costs.length == 0)
+            return 0;
+        for (int i = 1; i < costs.length; i++) {
+            costs[i][0] += Math.min(costs[i - 1][1], costs[i - 1][2]);
+            costs[i][1] += Math.min(costs[i - 1][0], costs[i - 1][2]);
+            costs[i][2] += Math.min(costs[i - 1][0], costs[i - 1][1]);
         }
-        return Math.min(red, Math.min(green, blue));
+        return Math.min(costs[costs.length - 1][0], Math.min(costs[costs.length - 1][1], costs[costs.length - 1][2]));
     }
 }
