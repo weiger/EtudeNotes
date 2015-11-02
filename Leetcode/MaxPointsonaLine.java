b200d7f5d1e17e52f04c47141334e6b5a112e3a1
Xiaomeng Yi <yixiaomeng@gmail.com>
2015-11-01 20:13:20 -0800

    Update MaxPointsonaLine.java

--- Leetcode/MaxPointsonaLine.java
+++ Leetcode/MaxPointsonaLine.java
@@ -7,46 +7,49 @@
  *     Point(int a, int b) { x = a; y = b; }
  * }
  */
-/*brutal O(n^2)
-1.注意扫描时避开基准
-2.扫描是记录完全相同的点
-3.注意K正无穷时候
-4.HashMap奇数，每次循环结束后注意清空
+/*
+1. Dup统计重和点
+2. Ver统计K为正无穷的点
+3. 其他点Hash统计，double 分0.0 和 -0.0
+4. 别忘清空Hash
 */
 public class Solution {
     public int maxPoints(Point[] points) {
-        if(points==null || points.length==0)
+        if (points == null || points.length == 0) {
             return 0;
-        HashMap<Double,Integer> map = new HashMap<Double,Integer>();
-        int max = 1;
-        double k;
-        for(int i=0;i<points.length;i++){
-            int count = 1;
-            map.clear();
-            for(int j=0;j<points.length;j++){
-                if(points[j]==points[i]){
-                    continue;
-                }
-                if(points[j].x==points[i].x && points[j].y==points[i].y){
-                    count++;continue;
-                }
-                if(points[j].x==points[i].x && points[j].y!=points[i].y){
-                    k = Integer.MAX_VALUE;
-                }else{
-                    k = ((double)points[j].y-(double)points[i].y)/((double)points[j].x-(double)points[i].x);
-                }
-                if(map.containsKey(k)){
-                    map.put(k,map.get(k)+1);
-                }else{
-                    map.put(k,1);
-                }
-            }
-            int tmp = 0;
-            for(Integer x:map.values()){
-                tmp = Math.max(x,tmp);
-            }
-            max = Math.max(max,tmp+count);
         }
-        return max;
+        HashMap<Double, Integer> map = new HashMap<>();
+        int res = 0;
+        for (int i = 0; i < points.length; i++) {
+            int vertical = 0;
+            int dup = 1;
+            for (int j = i + 1; j < points.length; j++) {
+                if (points[i].x == points[j].x) {
+                    if (points[i].y == points[j].y) {
+                        dup++;
+                    } else {
+                        vertical++;
+                    }
+                } else {
+                    double slope;
+                    if (points[j].y == points[i].y) {
+                        slope = 0.0;
+                    } else {
+                        slope = (1.0) * (points[j].y - points[i].y) / (points[j].x - points[i].x);
+                    }
+                    if (!map.containsKey(slope)) {
+                        map.put(slope, 1);
+                    } else {
+                        map.put(slope, map.get(slope) + 1);
+                    }
+                }
+            }
+            for (Integer val: map.values()) {
+                res = Math.max(res, dup + val);
+            }
+            res = Math.max(res, dup + vertical);
+            map.clear();
+        }
+        return res;
     }
 }
