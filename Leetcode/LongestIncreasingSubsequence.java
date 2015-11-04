0061ec945fdf1dac044b58db38c205fbcc24763e
Xiaomeng Yi <yixiaomeng@gmail.com>
2015-11-03 22:28:45 -0800

    Update LongestIncreasingSubsequence.java

--- Leetcode/LongestIncreasingSubsequence.java
+++ Leetcode/LongestIncreasingSubsequence.java
@@ -20,6 +20,26 @@ public class Solution {
 
 public class Solution {
     public int lengthOfLIS(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, 1);
+        int res = 0;
+        for (int i = 0 ; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] > nums[i]) {
+                    dp[j] = Math.max(dp[j], dp[i] + 1);
+                }
+            }
+            res = Math.max(res, dp[i]);
+        }
+        return res;
+    }
+}
+
+public class Solution {
+    public int lengthOfLIS(int[] nums) {
         if (nums == null) {
             return 0;
         }
