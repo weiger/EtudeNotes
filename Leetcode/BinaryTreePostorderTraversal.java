14ed8d071e716b3f4b1de5807cd35b809b85d8e7
Xiaomeng Yi <yixiaomeng@gmail.com>
2015-11-05 21:54:43 -0800

    Update BinaryTreePostorderTraversal.java

--- Leetcode/BinaryTreePostorderTraversal.java
+++ Leetcode/BinaryTreePostorderTraversal.java
@@ -1,81 +1,45 @@
-/*
- * Given a binary tree, return the postorder traversal of its nodes' values.
-
- * For example:
- * Given binary tree {1,#,2,3},
- *    1
- *     \
- *      2
- *     /
- *    3
- * return [3,2,1].
-
- * Note: Recursive solution is trivial, could you do it iteratively?
- */
-/**
- * Definition for binary tree
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
-public class BinaryTreePostorderTraversal {
-    public void helper(ArrayList<Integer> list, TreeNode root) {
-        if(root == null)
-            return;
-        if(root.left != null)
-            helper(list, root.left);
-        if(root.right != null)
-            helper(list, root.right);
-        list.add(root.val);
-    }
-
-    public ArrayList<Integer> postorderTraversal_1(TreeNode root) {
-        ArrayList<Integer> res = new ArrayList<Integer>();
+public class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
         helper(res, root);
         return res;
     }
-public class Solution {
-    public ArrayList<Integer> postorderTraversal(TreeNode root) {
-        ArrayList<Integer> res = new ArrayList<Integer>();
-        recfunc(res,root);
-        return res;
-    }
-    public void recfunc(ArrayList<Integer> AL,TreeNode root){
-        if(root!=null){
-            recfunc(AL,root.left);
-            recfunc(AL,root.right);
-            AL.add(root.val);
+    
+    public void helper(List<Integer> res, TreeNode p){
+        if (p != null) {
+            helper(res, p.left);
+            helper(res, p.right);
+            res.add(p.val);
         }
     }
 }
-/********************************************************************/ 
 
-    public ArrayList<Integer> postorderTraversal_2(TreeNode root) {
-        ArrayList<Integer> res = new ArrayList<Integer>();
-        if(root == null)
-            return res;
+public class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null) return res;
         Stack<TreeNode> stack = new Stack<TreeNode>();
         stack.push(root);
-        while(!stack.isEmpty()) {
-            TreeNode next =stack.peek();
-            if(next.left == root || next.right == root || (next.left == null && next.right == null)) {
+        TreeNode last = root;
+        while (!stack.isEmpty()) {
+            TreeNode cur = stack.peek();
+            if (cur.left == last || cur.right == last || (cur.left == null && cur.right == null)) {
+                res.add(cur.val);
                 stack.pop();
-                res.add(next.val);
-                root = next;
-            }
-            else {
-                if(next.right != null)
-                    stack.push(next.right);
-                if(next.left != null)
-                    stack.push(next.left);
+                last = cur;
+            } else {
+                if (cur.right != null) {
+                    stack.push(cur.right);
+                }
+                if (cur.left != null) {
+                    stack.push(cur.left);
+                }
             }
         }
         return res;
-    }   
+    }
 }
+
 //Morris
 public class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
@@ -106,30 +70,4 @@ public class Solution {
         }
         return res;
     } 
-    public void reverse(TreeNode from, TreeNode to)
-    {
-        if (from == to)
-            return;
-        TreeNode x = from, y = from.right, z;
-        while (true){
-            z = y.right;
-            y.right = x;
-            x = y;
-            y = z;
-            if (x == to)
-                break;
-        }
-    }
-    public void printReverse(TreeNode from, TreeNode to,ArrayList<Integer> res){
-        reverse(from, to);
-        
-        TreeNode p = to;
-        while (true){
-            res.add(p.val);
-            if (p == from)
-                break;
-            p = p.right;
-        }
-        reverse(to, from);
-    }
 }
