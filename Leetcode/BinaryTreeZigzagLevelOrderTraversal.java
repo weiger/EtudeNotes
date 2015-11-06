f643563ccbf94984decf4d7685b898d6b495d206
Xiaomeng Yi <yixiaomeng@gmail.com>
2015-11-05 22:12:43 -0800

    Update BinaryTreeZigzagLevelOrderTraversal.java

--- Leetcode/BinaryTreeZigzagLevelOrderTraversal.java
+++ Leetcode/BinaryTreeZigzagLevelOrderTraversal.java
@@ -1,52 +1,63 @@
-/*
- * Given a binary tree, return the zigzag level order traversal of its nodes' 
- * values. 
- * (ie, from left to right, then right to left for the next level and alternate 
- * between).
-
- * For example:
- * Given binary tree {3,9,20,#,#,15,7},
-
- *     3
- *    / \
- *   9  20
- *     /  \
- *    15   7
- * return its zigzag level order traversal as:
-
- * [
- *   [3],
- *   [20,9],
- *   [15,7]
- * ]
- */
-public class BinaryTreeZigzagLevelOrderTraversal {
-    public ArrayList<ArrayList<Integer>> zigzagLevelOrder(TreeNode root) {
-        ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();
-        if(root == null) 
-            return res;
-        ArrayList<Integer> saveNodeVal = new ArrayList<Integer>();
-        LinkedList<TreeNode> curLevel = new LinkedList<TreeNode>();
-        LinkedList<TreeNode> nextLevel = new LinkedList<TreeNode>();
-        curLevel.add(root);
-        boolean isL2R = true;
-        while(!curLevel.isEmpty()) {
-            TreeNode cur = curLevel.remove();
-            saveNodeVal.add(cur.val);
-            if(cur.left != null)
-                nextLevel.add(cur.left);
-            if(cur.right != null) 
-                nextLevel.add(cur.right);
-            if(curLevel.isEmpty()) {
-                if(!isL2R)
-                    Collections.reverse(saveNodeVal);
-                isL2R = !isL2R;
-                res.add(saveNodeVal);
-                curLevel = nextLevel;
-                nextLevel = new LinkedList<TreeNode>();
-                saveNodeVal = new ArrayList<Integer>();
+public class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) return res;
+        Queue<TreeNode> q = new LinkedList<>();
+        List<Integer> list = new ArrayList<>();
+        q.offer(root);
+        boolean flag = false;
+        while (!q.isEmpty()) {
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode cur = q.poll();
+                list.add(cur.val);
+                if (cur.left != null) {
+                    q.offer(cur.left);
+                }
+                if (cur.right != null) {
+                    q.offer(cur.right);
+                }
             }
-        }   
+            if (flag) {
+                Collections.reverse(list);
+            }
+            flag = !flag;
+            res.add(list);
+            list = new ArrayList<>();
+        }
+        return res;
+    }
+}
+
+public class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) return res;
+        Queue<TreeNode> curLevel = new LinkedList<>();
+        Queue<TreeNode> nextLevel = new LinkedList<>();
+        List<Integer> list = new ArrayList<>();
+        boolean flag = false;
+        curLevel.offer(root);
+        while(!curLevel.isEmpty()){
+            TreeNode cur = curLevel.poll();
+            list.add(cur.val);
+            if (cur.left != null) {
+                nextLevel.offer(cur.left);
+            }
+            if (cur.right != null) {
+                nextLevel.offer(cur.right);
+            }
+            if (curLevel.isEmpty()) {
+                curLevel = nextLevel;
+                nextLevel = new LinkedList<>();
+                if (flag) {
+                    Collections.reverse(list);
+                }
+                flag = !flag;
+                res.add(list);
+                list = new ArrayList<>();
+            }
+        }
         return res;
     }
 }
