58d04aebf5f7252e9cc1a0f23204cab70b3cfb82
Xiaomeng Yi <yixiaomeng@gmail.com>
2015-11-05 21:41:42 -0800

    Update BinaryTreePreorderTraversal.java

--- Leetcode/BinaryTreePreorderTraversal.java
+++ Leetcode/BinaryTreePreorderTraversal.java
@@ -1,59 +1,41 @@
-/*
- * Given a binary tree, return the preorder traversal of its nodes' values.
-
- * For example:
- * Given binary tree {1,#,2,3},
- *    1
- *     \
- *      2
- *     /
- *    3
- * return [1,2,3].
-
- * Note: Recursive solution is trivial, could you do it iteratively? 
- */
-/**
- * Definition for binary tree
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
-public BinaryTreePreorderTraversal {
-    public void helper(TreeNode root, ArrayList<Integer> list) {
-        if(root == null)
-            return;
-        list.add(root.val);
-        helper(root.left, list);
-        helper(root.right, list);
-    }
-
-    public ArrayList<Integer> preorderTraversal_1(TreeNode root) {
-        ArrayList<Integer> res = new ArrayList<Integer>();
-        helper(root, res);
+public class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        helper(res, root);
         return res;
     }
+    public void helper(List<Integer> res, TreeNode p){
+        if (p != null) {
+            res.add(p.val);
+            helper(res, p.left);
+            helper(res, p.right);
+        }
+    }
+}
 
-/******************************************************************/
-    
-    public ArrayList<Integer> preorderTraversal_2(TreeNode root) {
-        ArrayList<Integer> res = new ArrayList<Integer>();
+public class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
         Stack<TreeNode> stack = new Stack<TreeNode>();
-        while(!stack.isEmpty() || root != null) {
-            if(root != null) {
-                res.add(root.val);
-                if(root.right != null)
-                    stack.push(root.right);
-                root = root.left;
+        List<Integer> res = new ArrayList<>();
+        TreeNode cur = root;
+        while (cur != null || !stack.isEmpty()) {
+            if (cur != null) {
+                res.add(cur.val);
+                if (cur.right != null) {
+                    stack.push(cur.right);
+                }
+                cur = cur.left;
+            } else {
+                cur = stack.pop();
             }
-            else
-                root = stack.pop();
         }
         return res;
     }
 }
+
 //Morris
 public class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
