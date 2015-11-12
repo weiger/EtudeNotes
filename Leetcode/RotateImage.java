6c202d47c0b48ba78ddc18191a5bc122eb9d60f4
Xiaomeng Yi <yixiaomeng@gmail.com>
2015-11-12 01:52:48 -0800

    Update RotateImage.java

--- Leetcode/RotateImage.java
+++ Leetcode/RotateImage.java
@@ -1,36 +1,24 @@
-/*
- * You are given an n x n 2D matrix representing an image.
-
- * Rotate the image by 90 degrees (clockwise).
-
- * Follow up:
- * Could you do this in-place?
- */
- /*
- Write a example , follow it to modify index
- */
 public class Solution {
     public void rotate(int[][] matrix) {
-        int len = matrix.length;
-        for(int i =0;i<len-1;i++){
-            for(int j=0;j<len-i;j++){
+        int N = matrix.length;
+        for (int i = 0; i < N - 1; i++) {
+            for (int j = 0; j < N - i - 1; j++) {
                 int tmp = matrix[i][j];
-                matrix[i][j] = matrix[len-1-j][len-1-i];
-                matrix[len-1-j][len-1-i] = tmp;
+                matrix[i][j] = matrix[N - j - 1][N - i - 1];
+                matrix[N - j - 1][N - i - 1] = tmp;
             }
         }
-        for(int i=0;i<len/2;i++){
-            for(int j=0;j<len;j++){
+        for (int j = 0; j < N ; j++) {
+            for (int i = 0; i < N / 2; i++) {
                 int tmp = matrix[i][j];
-                matrix[i][j] = matrix[len-1-i][j];
-                matrix[len-1-i][j] = tmp;
+                matrix[i][j] =  matrix[N - 1 - i][j];
+                matrix[N - 1 - i][j] = tmp;
             }
         }
     }
 }
  
- 
-public class RotateImage {
+public class Solution {
     // two different rotating methods.
     public void rotate(int[][] matrix) {
         int n = matrix.length;
@@ -44,8 +32,6 @@ public class RotateImage {
             }
         }
     }
-
-/*************************** updated 2013/12/13 ******************************/
 
     public void rotate(int[][] matrix) {
         int len = matrix.length;
