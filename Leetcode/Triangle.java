c15a325343edbf0bc9bc4547be0f23ad550fa9bf
Xiaomeng Yi <yixiaomeng@gmail.com>
2015-11-12 02:03:46 -0800

    Update Triangle.java

--- Leetcode/Triangle.java
+++ Leetcode/Triangle.java
@@ -25,52 +25,52 @@ from left to right !!!
  */
  public class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
-        if(triangle == null || triangle.size() == 0)
-            return 0;
-        int[] dp = new int[triangle.get(triangle.size() - 1).size()]; 
-        for(int i = 0; i < dp.length; i++){
-            dp[i] = triangle.get(triangle.size() - 1).get(i);
+        int len = triangle.size();
+        int[][] dp = new int[len][len];
+        for (int i = 0; i < len; i++) {
+            dp[len - 1][i] = triangle.get(triangle.size() - 1).get(i);
         }
-        for(int i = triangle.size() - 2; i >=0 ; i--){
-            for(int j = 0; j < triangle.get(i).size(); j++){
-                dp[j] = triangle.get(i).get(j) + Math.min(dp[j],dp[j + 1]);
-            }
-        }
-        return dp[0];
-    }
-}
-
- public class Solution {
-    public int minimumTotal(List<List<Integer>> triangle) {
-        int[] dp = new int[triangle.size()];
-        for(int i=0;i<triangle.get(triangle.size()-1).size();i++){
-            dp[i] = triangle.get(triangle.size()-1).get(i);
-        }
-        for(int i=triangle.size()-2;i>=0;i--){    
-            for(int j=0;j<triangle.get(i+1).size()-1;j++){  //boundary!!
-                dp[j] = triangle.get(i).get(j) + Math.min(dp[j],dp[j+1]);
-            }
-        }
-        return dp[0];
-    }
-}
- 
- 
- 
-import java.util.*;
-
-public class Triangle {
-    public int minimumTotal(ArrayList<ArrayList<Integer>> triangle) {
-        int row = triangle.size();
-        int[][] dp = new int[row][];
-        for(int i = 0; i < row; i++) 
-            dp[i] = new int[i + 1];
-        for(int i = 0; i < row; i++)
-            dp[row - 1][i] = triangle.get(row - 1).get(i);
-        for(int i = row - 2; i >= 0; i--) {
-            for(int j = 0; j <= i; j++)
+        
+        for (int i = len - 2; i >= 0; i--) {
+            for (int j = 0; j < triangle.get(i).size(); j++) {
                 dp[i][j] = triangle.get(i).get(j) + Math.min(dp[i + 1][j], dp[i + 1][j + 1]);
+            }
         }
         return dp[0][0];
     }
 }
+
+public class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        if(triangle == null || triangle.size() == 0)
+            return 0;
+        int[] dp = new int[triangle.size()];
+        for(int i = dp.length - 1; i >= 0; i--){
+            dp[i] = triangle.get(triangle.size() - 1).get(i);
+        }
+        for(int i = triangle.size() - 2; i >=0 ; i--){
+            for(int j = 0; j < triangle.get(i).size(); j++){
+                dp[j] = Math.min(dp[j], dp[j + 1]) + triangle.get(i).get(j);
+            }
+        }
+        return dp[0];
+    }
+}
+
+public class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        if(triangle == null || triangle.size() == 0)
+            return 0;
+        int[] dp = new int[triangle.size()];
+        for(int i = dp.length - 1; i >= 0; i--){
+            dp[i] = triangle.get(triangle.size() - 1).get(i);
+        }
+        for(int i = triangle.size() - 2; i >=0 ; i--){
+            for(int j = 0; j < triangle.get(i).size(); j++){
+                dp[j] = Math.min(dp[j], dp[j + 1]) + triangle.get(i).get(j);
+            }
+        }
+        return dp[0];
+    }
+}
+
