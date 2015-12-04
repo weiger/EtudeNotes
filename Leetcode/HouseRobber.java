7ccfbfb5f3d9fad0dcf2ef79815791fb479c81b9
Xiaomeng Yi <yixiaomeng@gmail.com>
2015-12-03 22:57:36 -0800

    Create HouseRobber.java

---
+++ Leetcode/HouseRobber.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public int rob(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+        if (nums.length == 1) return nums[0];
+        int len = nums.length;
+        int[] dp = new int[len];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+        for (int i = 2; i < len; i++) {
+            dp[i] = Math.max(dp[i - 1], nums[i] + dp[i - 2]);
+        }
+        return dp[len - 1];
+    }
+}
