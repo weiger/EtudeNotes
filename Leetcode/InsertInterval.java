c6ff434ba88a97240b005978a4306211318e9184
Xiaomeng Yi <yixiaomeng@gmail.com>
2015-12-03 23:50:14 -0800

    Update InsertInterval.java

--- Leetcode/InsertInterval.java
+++ Leetcode/InsertInterval.java
@@ -14,3 +14,25 @@ public class Solution {
         return intervals;
     }
 }
+// no modification on original list
+public class Solution {
+    public List<Interval> insert(List<Interval> intervals, Interval newInterval) {
+        List<Interval> res = new ArrayList<>();
+        for (Interval cur: intervals) {
+            if (newInterval == null || newInterval.start > cur.end) {
+                res.add(cur);
+            } else if (newInterval.end < cur.start) {
+                res.add(newInterval);
+                res.add(cur);
+                newInterval = null;
+            } else {
+                newInterval.start = Math.min(newInterval.start, cur.start);
+                newInterval.end = Math.max(newInterval.end, cur.end);
+            }
+        }
+        if (newInterval != null) {
+            res.add(newInterval);
+        }
+        return res;
+    }
+}
