72bcfbd5434cb31d136cff14942fb7cecb4c3625
Xiaomeng Yi <yixiaomeng@gmail.com>
2015-12-07 19:55:23 -0800

    Update IntersectionofTwoLinkedLists.java

--- Leetcode/IntersectionofTwoLinkedLists.java
+++ Leetcode/IntersectionofTwoLinkedLists.java
@@ -94,3 +94,21 @@ public class Solution {
         return cur1;
     }
 }
+
+
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) return null;
+        ListNode cur1 = headA;
+        ListNode cur2 = headB;
+        while (true) {
+            if (cur1 == cur2) return cur1;
+            cur1 = cur1.next;
+            cur2 = cur2.next;
+            if (cur1 == cur2) return cur1;
+            if (cur1 == null) cur1 = headB;
+            if (cur2 == null) cur2 = headA;
+            if (cur1 == cur2) return cur1;
+        }
+    }
+}
