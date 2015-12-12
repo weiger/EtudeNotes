8a3562bf873d5cb8d8195051c9591e7a1b3c9913
Xiaomeng Yi <yixiaomeng@gmail.com>
2015-12-11 16:55:19 -0800

    Update SameTree.java

--- Leetcode/SameTree.java
+++ Leetcode/SameTree.java
@@ -12,17 +12,6 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
- public class Solution {
-    public boolean isSameTree(TreeNode p, TreeNode q) {
-        if(p==null && q==null) return true;
-        if(p==null || q==null) return false;
-        if(p.val==q.val)
-            return isSameTree(p.left,q.left)&&isSameTree(p.right,q.right);
-        else
-            return false;
-    }
-}
-
 public class SameTree {
     public boolean isSameTree(TreeNode p, TreeNode q) {
         if(p == null && q == null)
@@ -35,28 +24,46 @@ public class SameTree {
 
 public class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        Queue<TreeNode> queue1 = new LinkedList<TreeNode>();
-        Queue<TreeNode> queue2 = new LinkedList<TreeNode>();
-        queue1.offer(p);
-        queue2.offer(q);
-        while (!queue1.isEmpty() && !queue2.isEmpty()) {
-            TreeNode q1 = queue1.poll();
-            TreeNode q2 = queue2.poll();
-            if (q1 == null && q2 == null) {
-                continue;
-            } else if (q1 == null || q2 == null) {
-                return false;
-            } else if (q1.val != q2.val) {
-                return false;
-            }
-            queue1.offer(q1.left);
-            queue2.offer(q2.left);
-            queue1.offer(q1.right);
-            queue2.offer(q2.right);
+        if (p == null && q == null) return true;
+        if (p == null || q == null) return false;
+        Queue<TreeNode> q1 = new LinkedList<>();
+        Queue<TreeNode> q2 = new LinkedList<>();
+        q1.offer(p);
+        q2.offer(q);
+        while (!q1.isEmpty() && !q2.isEmpty()) {
+            TreeNode cur1 = q1.poll();
+            TreeNode cur2 = q2.poll();
+            if (cur1 == null && cur2 == null) continue;
+            if (cur1 == null || cur2 == null || cur1.val != cur2.val) return false;
+            q1.offer(cur1.left);
+            q1.offer(cur1.right);
+            q2.offer(cur2.left);
+            q2.offer(cur2.right);
         }
-        if (!queue1.isEmpty() || !queue2.isEmpty()) {
-            return false;
+        if (!q1.isEmpty() || !q2.isEmpty()) return false;
+        return true;
+    }
+}
+
+public class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        if (p == null || q == null) return false;
+        Stack<TreeNode> s1 = new Stack<>();
+        Stack<TreeNode> s2 = new Stack<>();
+        s1.push(p);
+        s2.push(q);
+        while (!s1.isEmpty() && !s2.isEmpty()) {
+            TreeNode cur1 = s1.pop();
+            TreeNode cur2 = s2.pop();
+            if (cur1 == null && cur2 == null) continue;
+            if (cur1 == null || cur2 == null || cur1.val != cur2.val) return false;
+            s1.push(cur1.left);
+            s2.push(cur2.left);
+            s1.push(cur1.right);
+            s2.push(cur2.right);
         }
+        if (!s1.isEmpty() || !s2.isEmpty()) return false;
         return true;
     }
 }
