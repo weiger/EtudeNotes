eb268f7852ab54309811d189a676d1909f915f4f
Xiaomeng Yi <yixiaomeng@gmail.com>
2015-12-26 11:33:25 -0800

    Create GrabStocks.java

---
+++ DesignPattern/Observer/GrabStocks.java
@@ -0,0 +1,27 @@
+package Observer;
+
+public class GrabStocks {
+    public static void main(String[] args) {
+        StockGrabber stockGrabber = new StockGrabber();
+
+        StockObserver observer1 = new StockObserver(stockGrabber);
+
+        stockGrabber.setIBMPrice(197.00);
+        stockGrabber.setAAPLPrice(677.60);
+        stockGrabber.setGOOGPrice(676.40);
+
+        StockObserver observer2 = new StockObserver(stockGrabber);
+
+        stockGrabber.setIBMPrice(197.00);
+        stockGrabber.setAAPLPrice(677.60);
+        stockGrabber.setGOOGPrice(676.40);
+
+        stockGrabber.setIBMPrice(197.00);
+        stockGrabber.setAAPLPrice(677.60);
+        stockGrabber.setGOOGPrice(676.40);
+
+        new Thread(new GetTheStock(stockGrabber, "IBM", 197.00)).start();
+        new Thread(new GetTheStock(stockGrabber, "AAPL", 677.60)).start();
+        new Thread(new GetTheStock(stockGrabber, "GOOG", 676.40)).start();
+    }
+}
